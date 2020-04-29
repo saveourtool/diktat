37f4aae57b4753f495d8ce9723996e7dee1b4e2e
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2020-04-30 01:56:56 +0300

    Fixing bug with incorrect package name parsing, adding simple autofix for it, changing tests

--- ktlint-ruleset-huawei/src/main/kotlin/rri/fixbot/ruleset/huawei/huawei/utils/AstNodeUtils.kt
+++ ktlint-ruleset-huawei/src/main/kotlin/rri/fixbot/ruleset/huawei/huawei/utils/AstNodeUtils.kt
@@ -1,7 +1,9 @@
 package rri.fixbot.ruleset.huawei.huawei.utils
 
 import com.pinterest.ktlint.core.ast.ElementType
+import com.pinterest.ktlint.core.ast.isLeaf
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.com.intellij.psi.tree.IElementType
 
 fun ASTNode.checkLength(range: IntRange): Boolean = this.textLength in range
 
@@ -28,3 +30,19 @@ fun ASTNode.isValProperty() =
 fun ASTNode.isVarProperty() =
     this.getChildren(null)
         .any { it.elementType == ElementType.VAR_KEYWORD }
+
+/**
+ * This util method does tree traversal and stores to the result all tree leaf node of particular type (elementType)
+ */
+fun ASTNode.getAllLLeafsWithSpecificType(elementType: IElementType, result: MutableList<ASTNode>) {
+    // if statements here have the only right order - don't change it
+    if (this.isLeaf()) {
+        if (this.elementType == elementType) {
+            result.add(this)
+        }
+    } else {
+        this.getChildren(null).forEach {
+            it.getAllLLeafsWithSpecificType(elementType, result)
+        }
+    }
+}
