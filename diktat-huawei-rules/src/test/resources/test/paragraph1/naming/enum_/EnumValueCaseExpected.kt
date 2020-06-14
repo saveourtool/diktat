9d597cd0c00611ad61dd3c3db89d10b4c78a84fe
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2020-06-15 01:38:25 +0300

    Changes to format fixing. Added aggressive autofix for Enum values format
    ### What's done:
    1) Enum values case format fixing
    2) Tests

---
+++ diktat-huawei-rules/src/test/resources/test/paragraph1/naming/enum_/EnumValueCaseExpected.kt
@@ -0,0 +1,10 @@
+package com.huawei.test.resources.test.paragraph1.naming.enum_
+
+enum class EnumValueCaseTest {
+    PA_SC_SAL_L,
+    PASC_ASL_F,
+    START_PSAAA_DFE,
+    NAME_MYA_SAY_R,
+    NAME_MYA_SAY_R_
+}
+
