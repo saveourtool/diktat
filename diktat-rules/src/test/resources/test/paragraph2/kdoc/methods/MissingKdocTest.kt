9debf5a5525596e482cfae36d29008de51dd74ab
Peter Trifanov <peter.trifanov@mail.ru>
2020-06-25 15:07:09 +0800

    Fixing function Kdoc related bugs
    ### What's done:
    * Introduced separate warning to suggest Kdoc template for a function, ensure it is not empty, properly indent
    * Fixing missing @param in existing KDoc for each parameter
    * Fixing missing @return tag in existing KDoc
    * Fixing missing @throws tag in existing KDoc for every exception thrown explicitly in this method's body
    * Introduced method in KDocUtils to insert tag to existing KDoc

---
+++ diktat-rules/src/test/resources/test/paragraph2/kdoc/methods/MissingKdocTest.kt
@@ -0,0 +1,8 @@
+package test.paragraph2.kdoc
+
+class Example {
+    fun addInts(a: Int, b: Int): Int {
+        if (false) throw IllegalStateException()
+        return a + b
+    }
+}
---
+++ diktat-rules/src/test/resources/test/paragraph2/kdoc/methods/ThrowsTagInsertionTest.kt
@@ -0,0 +1,43 @@
+package test.paragraph2.kdoc.methods
+
+class Example {
+    /**
+     * Lorem ipsum
+     */
+    fun test1(): Unit {
+        throw IllegalStateException("Lorem ipsum")
+    }
+
+    /**
+     * Lorem ipsum
+     * @param a integer parameter
+     */
+    fun test2(a: Int): Unit  {
+        throw IllegalStateException("Lorem ipsum")
+    }
+
+    /**
+     * Lorem ipsum
+     * @param a integer parameter
+     * @return integer value
+     */
+    fun test3(a: Int): Int  {
+        throw IllegalStateException("Lorem ipsum")
+    }
+
+    /**
+     * Lorem ipsum
+     * @return integer value
+     */
+    fun test4(): Int  {
+        throw IllegalStateException("Lorem ipsum")
+    }
+
+    /**
+     * Lorem ipsum
+     */
+    fun test5() {
+        if (true) throw IllegalStateException("Lorem ipsum")
+        else throw IllegalAccessException("Dolor sit amet")
+    }
+}
