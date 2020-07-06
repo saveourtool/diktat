7be2237b1ee779f4bd8b5302794b2d5bc9850889
Denis Kumar <qbextted0@gmail.com>
2020-07-06 21:38:44 +0300

    Added tests for FileSize rule
    ### What's done:
    * Added tests

---
+++ diktat-rules/src/test/resources/test/paragraph3/src/main/A/FileSizeA.kt
@@ -0,0 +1,9 @@
+package test.paragraph3.src.main.A
+
+class A {
+
+    fun tester(a: Int) = Unit
+
+
+    fun testER(a: Int) = Unit
+}
---
+++ diktat-rules/src/test/resources/test/paragraph3/src/main/C/FileSizeC.kt
@@ -0,0 +1,7 @@
+package test.paragraph3.src.main.C
+
+class C {
+
+    fun tester(a: Int) = Unit
+
+}
