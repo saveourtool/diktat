70d32578c92cba83f8a581a1af7f436bc47eb5e3
petertrr <peter.trifanov@mail.ru>
2020-07-06 10:46:01 +0300

    Fixed #14, added other fixes
    ### What's done:
    * Fixed #14
    * Removed unused pom.xml
    * Added execution id for detekt plugin, changed workflow detekt.yml
    * Added explicit version for maven-compiler-plugin

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/KdocMethods.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/KdocMethods.kt
@@ -1,5 +1,27 @@
 package org.cqfn.diktat.ruleset.rules
 
+import com.pinterest.ktlint.core.KtLint
+import com.pinterest.ktlint.core.Rule
+import com.pinterest.ktlint.core.ast.ElementType.BINARY_EXPRESSION
+import com.pinterest.ktlint.core.ast.ElementType.BLOCK
+import com.pinterest.ktlint.core.ast.ElementType.CALLABLE_REFERENCE_EXPRESSION
+import com.pinterest.ktlint.core.ast.ElementType.CALL_EXPRESSION
+import com.pinterest.ktlint.core.ast.ElementType.COLLECTION_LITERAL_EXPRESSION
+import com.pinterest.ktlint.core.ast.ElementType.FUN
+import com.pinterest.ktlint.core.ast.ElementType.FUN_KEYWORD
+import com.pinterest.ktlint.core.ast.ElementType.KDOC
+import com.pinterest.ktlint.core.ast.ElementType.KDOC_TAG_NAME
+import com.pinterest.ktlint.core.ast.ElementType.KDOC_TEXT
+import com.pinterest.ktlint.core.ast.ElementType.LAMBDA_EXPRESSION
+import com.pinterest.ktlint.core.ast.ElementType.MODIFIER_LIST
+import com.pinterest.ktlint.core.ast.ElementType.REFERENCE_EXPRESSION
+import com.pinterest.ktlint.core.ast.ElementType.SAFE_ACCESS_EXPRESSION
+import com.pinterest.ktlint.core.ast.ElementType.THROW
+import com.pinterest.ktlint.core.ast.ElementType.TYPE_REFERENCE
+import com.pinterest.ktlint.core.ast.ElementType.WHEN_CONDITION_WITH_EXPRESSION
+import com.pinterest.ktlint.core.ast.ElementType.WHITE_SPACE
+import com.pinterest.ktlint.core.ast.prevSibling
+import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.cqfn.diktat.ruleset.constants.Warnings.KDOC_WITHOUT_PARAM_TAG
 import org.cqfn.diktat.ruleset.constants.Warnings.KDOC_WITHOUT_RETURN_TAG
 import org.cqfn.diktat.ruleset.constants.Warnings.KDOC_WITHOUT_THROWS_TAG
@@ -13,21 +35,6 @@ import org.cqfn.diktat.ruleset.utils.insertTagBefore
 import org.cqfn.diktat.ruleset.utils.isAccessibleOutside
 import org.cqfn.diktat.ruleset.utils.kDocTags
 import org.cqfn.diktat.ruleset.utils.parameterNames
-import com.pinterest.ktlint.core.KtLint
-import com.pinterest.ktlint.core.Rule
-import com.pinterest.ktlint.core.ast.ElementType
-import com.pinterest.ktlint.core.ast.ElementType.BLOCK
-import com.pinterest.ktlint.core.ast.ElementType.FUN
-import com.pinterest.ktlint.core.ast.ElementType.FUN_KEYWORD
-import com.pinterest.ktlint.core.ast.ElementType.KDOC
-import com.pinterest.ktlint.core.ast.ElementType.KDOC_TAG_NAME
-import com.pinterest.ktlint.core.ast.ElementType.KDOC_TEXT
-import com.pinterest.ktlint.core.ast.ElementType.MODIFIER_LIST
-import com.pinterest.ktlint.core.ast.ElementType.THROW
-import com.pinterest.ktlint.core.ast.ElementType.TYPE_REFERENCE
-import com.pinterest.ktlint.core.ast.ElementType.WHITE_SPACE
-import com.pinterest.ktlint.core.ast.prevSibling
-import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
@@ -44,10 +51,8 @@ import org.jetbrains.kotlin.kdoc.psi.impl.KDocTag
 @Suppress("ForbiddenComment")
 class KdocMethods : Rule("kdoc-methods") {
     // expression body of function can have a lot of 'ElementType's, this list might be not full
-    private val expressionBodyTypes = setOf(ElementType.BINARY_EXPRESSION, ElementType.CALL_EXPRESSION,
-        ElementType.LAMBDA_EXPRESSION, ElementType.REFERENCE_EXPRESSION, ElementType.CALLABLE_REFERENCE_EXPRESSION,
-        ElementType.SAFE_ACCESS_EXPRESSION, ElementType.WHEN_CONDITION_WITH_EXPRESSION,
-        ElementType.COLLECTION_LITERAL_EXPRESSION)
+    private val expressionBodyTypes = setOf(BINARY_EXPRESSION, CALL_EXPRESSION, LAMBDA_EXPRESSION, REFERENCE_EXPRESSION,
+            CALLABLE_REFERENCE_EXPRESSION, SAFE_ACCESS_EXPRESSION, WHEN_CONDITION_WITH_EXPRESSION, COLLECTION_LITERAL_EXPRESSION)
 
     private lateinit var configRules: List<RulesConfig>
     private lateinit var emitWarn: ((offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit)
@@ -67,87 +72,38 @@ class KdocMethods : Rule("kdoc-methods") {
         }
     }
 
-    @Suppress("LongMethod")
     private fun checkSignatureDescription(node: ASTNode) {
         val kDoc = node.getFirstChildWithType(KDOC)
         val kDocTags = kDoc?.kDocTags()
+        val name = node.getIdentifierName()!!.text
 
         val missingParameters = getMissingParameters(node, kDocTags)
 
         val explicitlyThrownExceptions = getExplicitlyThrownExceptions(node)
         val missingExceptions = explicitlyThrownExceptions
-            .minus(kDocTags
-                ?.filter { it.knownTag == KDocKnownTag.THROWS }
-                ?.map { it.getSubjectName() }
-                ?.toSet() ?: setOf()
-            )
+                .minus(kDocTags
+                        ?.filter { it.knownTag == KDocKnownTag.THROWS }
+                        ?.mapNotNull { it.getSubjectName() }
+                        ?.toSet() ?: setOf()
+                )
 
         val paramCheckFailed = missingParameters.isNotEmpty()
         val returnCheckFailed = checkReturnCheckFailed(node, kDocTags)
         val throwsCheckFailed = missingExceptions.isNotEmpty()
 
-        val name = node.getIdentifierName()!!.text
-        if (paramCheckFailed) {
-            KDOC_WITHOUT_PARAM_TAG.warnAndFix(configRules, emitWarn, isFixMode,
-                "$name (${missingParameters.joinToString()})", node.startOffset) {
-                val beforeTag = kDocTags?.find { it.knownTag == KDocKnownTag.RETURN }
-                     ?: kDocTags?.find { it.knownTag == KDocKnownTag.THROWS }
-                missingParameters.forEach {
-                    kDoc?.insertTagBefore(beforeTag?.node) {
-                        addChild(LeafPsiElement(KDOC_TAG_NAME, "@param"))
-                        addChild(PsiWhiteSpaceImpl(" "))
-                        addChild(LeafPsiElement(KDOC_TEXT, it))
-                    }
-                }
-            }
-        }
-        if (returnCheckFailed) {
-            KDOC_WITHOUT_RETURN_TAG.warnAndFix(configRules, emitWarn, isFixMode, name, node.startOffset) {
-                val beforeTag = kDocTags?.find { it.knownTag == KDocKnownTag.THROWS }
-                kDoc?.insertTagBefore(beforeTag?.node) {
-                    addChild(LeafPsiElement(KDOC_TAG_NAME, "@return"))
-                }
-            }
-        }
-        if (throwsCheckFailed) {
-            KDOC_WITHOUT_THROWS_TAG.warnAndFix(configRules, emitWarn, isFixMode,
-                "$name (${missingExceptions.joinToString()})", node.startOffset) {
-                explicitlyThrownExceptions.forEach {
-                    kDoc?.insertTagBefore(null) {
-                        addChild(LeafPsiElement(KDOC_TAG_NAME, "@throws"))
-                        addChild(LeafPsiElement(KDOC_TEXT, " "))
-                        addChild(LeafPsiElement(KDOC_TEXT, it))
-                    }
-                }
-            }
-        }
+        if (paramCheckFailed) handleParamCheck(node, name, kDoc, missingParameters, kDocTags)
+        if (returnCheckFailed) handleReturnCheck(node, name, kDoc, kDocTags)
+        if (throwsCheckFailed) handleThrowsCheck(node, name, kDoc, missingExceptions)
 
         // if no tag failed, we have too little information to suggest KDoc - it would just be empty
         val anyTagFailed = paramCheckFailed || returnCheckFailed || throwsCheckFailed
-        if (kDoc == null && anyTagFailed) {
-            MISSING_KDOC_ON_FUNCTION.warnAndFix(configRules, emitWarn, isFixMode,
-                node.getIdentifierName()!!.text, node.startOffset) {
-                val indent = node.prevSibling { it.elementType == WHITE_SPACE }?.text
-                    ?.substringAfterLast("\n")?.count { it == ' ' } ?: 0
-                val kDocTemplate = "/**\n" +
-                    (missingParameters.joinToString("") { " * @param $it\n" } +
-                        (if (returnCheckFailed) " * @return\n" else "") +
-                        explicitlyThrownExceptions.joinToString("") { " * @throws $it\n" } +
-                        " */\n"
-                        ).prependIndent(" ".repeat(indent))
-
-                // we must ensure that KDoc is inserted before `fun` keyword
-                val methodNode = node.getFirstChildWithType(FUN_KEYWORD)
-                // fixme could be added as proper CompositeElement
-                node.addChild(LeafPsiElement(KDOC, kDocTemplate), methodNode)
-            }
-        }
+        if (kDoc == null && anyTagFailed) addKdocTemplate(node, name, missingParameters, explicitlyThrownExceptions, returnCheckFailed)
     }
 
     private fun getMissingParameters(node: ASTNode, kDocTags: Collection<KDocTag>?): Collection<String?> {
         val parameterNames = node.parameterNames()
         val kDocParameterNames = kDocTags?.filter { it.knownTag == KDocKnownTag.PARAM }
-            ?.map { it.getSubjectName() }
+                ?.map { it.getSubjectName() }
         return if (parameterNames == null || parameterNames.isEmpty()) {
             listOf()
         } else if (kDocParameterNames != null && kDocParameterNames.isNotEmpty()) {
@@ -164,7 +120,7 @@ class KdocMethods : Rule("kdoc-methods") {
         val isFunWithExpressionBody = expressionBodyTypes.any { node.hasChildOfType(it) }
         val hasReturnKDoc = kDocTags != null && kDocTags.hasKnownKDocTag(KDocKnownTag.RETURN)
         return (hasExplicitNotUnitReturnType || isFunWithExpressionBody && !hasExplicitUnitReturnType)
-            && !hasReturnKDoc
+                && !hasReturnKDoc
     }
 
     private fun getExplicitlyThrownExceptions(node: ASTNode): Set<String> {
@@ -172,8 +128,80 @@ class KdocMethods : Rule("kdoc-methods") {
         val throwKeywords = codeBlock?.findAllNodesWithSpecificType(THROW)
         return throwKeywords?.map {
             // fixme probably `throws` can have other expression types
-            it.findChildByType(ElementType.CALL_EXPRESSION)
-                ?.findChildByType(ElementType.REFERENCE_EXPRESSION)?.text!!
+            it.findChildByType(CALL_EXPRESSION)
+                    ?.findChildByType(REFERENCE_EXPRESSION)?.text!!
         }?.toSet() ?: setOf()
+    }
+
+    private fun handleParamCheck(node: ASTNode,
+                                 name: String,
+                                 kDoc: ASTNode?,
+                                 missingParameters: Collection<String?>,
+                                 kDocTags: Collection<KDocTag>?) {
+        KDOC_WITHOUT_PARAM_TAG.warnAndFix(configRules, emitWarn, isFixMode,
+                "$name (${missingParameters.joinToString()})", node.startOffset) {
+            val beforeTag = kDocTags?.find { it.knownTag == KDocKnownTag.RETURN }
+                    ?: kDocTags?.find { it.knownTag == KDocKnownTag.THROWS }
+            missingParameters.forEach {
+                kDoc?.insertTagBefore(beforeTag?.node) {
+                    addChild(LeafPsiElement(KDOC_TAG_NAME, "@param"))
+                    addChild(PsiWhiteSpaceImpl(" "))
+                    addChild(LeafPsiElement(KDOC_TEXT, it))
+                }
+            }
+        }
+    }
+
+    private fun handleReturnCheck(node: ASTNode,
+                                  name: String,
+                                  kDoc: ASTNode?,
+                                  kDocTags: Collection<KDocTag>?
+    ) {
+        KDOC_WITHOUT_RETURN_TAG.warnAndFix(configRules, emitWarn, isFixMode, name, node.startOffset) {
+            val beforeTag = kDocTags?.find { it.knownTag == KDocKnownTag.THROWS }
+            kDoc?.insertTagBefore(beforeTag?.node) {
+                addChild(LeafPsiElement(KDOC_TAG_NAME, "@return"))
+            }
+        }
+    }
+
+    private fun handleThrowsCheck(node: ASTNode,
+                                  name: String,
+                                  kDoc: ASTNode?,
+                                  missingExceptions: Collection<String>
+    ) {
+        KDOC_WITHOUT_THROWS_TAG.warnAndFix(configRules, emitWarn, isFixMode,
+                "$name (${missingExceptions.joinToString()})", node.startOffset) {
+            missingExceptions.forEach {
+                kDoc?.insertTagBefore(null) {
+                    addChild(LeafPsiElement(KDOC_TAG_NAME, "@throws"))
+                    addChild(LeafPsiElement(KDOC_TEXT, " "))
+                    addChild(LeafPsiElement(KDOC_TEXT, it))
+                }
+            }
+        }
+    }
+
+    private fun addKdocTemplate(node: ASTNode,
+                                name: String,
+                                missingParameters: Collection<String?>,
+                                explicitlyThrownExceptions: Collection<String>,
+                                returnCheckFailed: Boolean
+    ) {
+        MISSING_KDOC_ON_FUNCTION.warnAndFix(configRules, emitWarn, isFixMode, name, node.startOffset) {
+            val indent = node.prevSibling { it.elementType == WHITE_SPACE }?.text
+                    ?.substringAfterLast("\n")?.count { it == ' ' } ?: 0
+            val kDocTemplate = "/**\n" +
+                    (missingParameters.joinToString("") { " * @param $it\n" } +
+                            (if (returnCheckFailed) " * @return\n" else "") +
+                            explicitlyThrownExceptions.joinToString("") { " * @throws $it\n" } +
+                            " */\n"
+                            ).prependIndent(" ".repeat(indent))
+
+            // we must ensure that KDoc is inserted before `fun` keyword
+            val methodNode = node.getFirstChildWithType(FUN_KEYWORD)
+            // fixme could be added as proper CompositeElement
+            node.addChild(LeafPsiElement(KDOC, kDocTemplate), methodNode)
+        }
     }
 }
