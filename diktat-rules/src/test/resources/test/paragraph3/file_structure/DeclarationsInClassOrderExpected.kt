25ecc3b2082df6529177205cd4b371d9eb35f5a5
Peter Trifanov <peter.trifanov@mail.ru>
2020-07-20 12:03:42 +0300

    Implementation of recommendation 3.2 (#71)
    * Implementation of recommendation 3.2
    
    ### What's done:
    * Introduced ASTNodeUtils#checkOrder, added to FileStructureRule
    * Introduced ClassLikeStructuresOrderRule.kt
    * Refactored lintMethod in TestUtils to make assertion errors more readable
    * Added tests on checkers and fixers
    * Updated available-rules.md
    * Updated rules-config.json

---
+++ diktat-rules/src/test/resources/test/paragraph3/file_structure/DeclarationsInClassOrderExpected.kt
@@ -0,0 +1,36 @@
+package test.paragraph3.file_structure
+
+class Example {
+    private val log = LoggerFactory.getLogger(Example.javaClass)
+
+    /**
+     * Dolor sit amet
+     */
+    private val BAZ = 44
+private val BAR = 43
+
+    // lorem ipsum
+    private val FOO = 42
+    private lateinit var lateFoo: Int
+    init {
+        bar()
+    }
+
+    constructor(baz: Int)
+
+    fun foo() {
+        val nested = Nested()
+    }
+
+    class Nested {
+        val nestedFoo = 43
+    }
+
+    companion object {
+        private const val ZERO = 0
+        private var i = 0
+    }
+
+    class UnusedNested { }
+
+    }
