2e0e30cf3f6e5c87c4c91e003b1071c48242c7fe
kentr0w <qbextted0@gmail.com>
2020-07-24 15:48:50 +0300

    Tests to EnumValueCase
    ### What's done:
      Remove config list

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter1/EnumValueCaseTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter1/EnumValueCaseTest.kt
@@ -1,14 +1,11 @@
 package org.cqfn.diktat.ruleset.chapter1
 
-import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.cqfn.diktat.ruleset.rules.IdentifierNaming
 import org.cqfn.diktat.util.FixTestBase
 import org.junit.Test
 
-class EnumValueCaseTest : FixTestBase ("test/paragraph1/naming", IdentifierNaming(),
-        listOf(
-                RulesConfig("ENUM_VALUE", true, mapOf()))
-){
+class EnumValueCaseTest : FixTestBase ("test/paragraph1/naming", IdentifierNaming()){
+
     @Test
     fun `incorrect enum value (fix)`() {
         fixAndCompare("enum_/EnumValueCaseExpected.kt", "enum_/EnumValueCaseTest.kt")
