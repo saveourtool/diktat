146608c2206125b73533f976b3baeb04c8d45c40
kentr0w <qbextted0@gmail.com>
2020-07-24 14:17:02 +0300

    Tests to EnumValueCase
    ### What's done:
      Add test to check fix

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter1/EnumValueCaseTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter1/EnumValueCaseTest.kt
@@ -1,2 +1,16 @@
 package org.cqfn.diktat.ruleset.chapter1
 
+import org.cqfn.diktat.common.config.rules.RulesConfig
+import org.cqfn.diktat.ruleset.rules.IdentifierNaming
+import org.cqfn.diktat.util.FixTestBase
+import org.junit.Test
+
+class EnumValueCaseTest : FixTestBase ("test/paragraph1/naming", IdentifierNaming(),
+        listOf(
+                RulesConfig("ENUM_VALUE", true, mapOf()))
+){
+    @Test
+    fun `incorrect enum value (fix)`() {
+        fixAndCompare("enum_/EnumValueCaseExpected.kt", "enum_/EnumValueCaseTest.kt")
+    }
+}
