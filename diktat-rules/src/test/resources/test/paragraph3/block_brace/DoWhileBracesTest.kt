6fa1146933663dfe8361588d90647ac47b26e767
kentr0w <38314854+kentr0w@users.noreply.github.com>
2020-07-24 14:50:50 +0300

    Rule 3.4 Block braces structure - correct errors (#99)
    * Rule 3.4 Block braces structure - correct errors
    
    ### What's done:
      Made rule correct errors
      Covered more code. Fixed bug with middle brace

---
+++ diktat-rules/src/test/resources/test/paragraph3/block_brace/DoWhileBracesTest.kt
@@ -0,0 +1,28 @@
+package test.paragraph3.block_brace
+
+fun foo1() {
+    do {
+        println("hello")
+    } while (x > 0)
+}
+
+fun foo2() {
+    do{
+        println("hello")
+    } while (y > 0)
+}
+
+fun foo3() {
+    do
+    {
+        println("hellp")
+    }while (z > 0)
+}
+
+fun foo4() {
+    do
+    {
+        println("hellp")
+    }
+    while (l > 0)
+}
---
+++ diktat-rules/src/test/resources/test/paragraph3/block_brace/IfElseBracesTest.kt
@@ -0,0 +1,34 @@
+package test.paragraph3.block_brace
+
+fun foo1() {
+    if (x > 0) { foo() } else { zoo() }
+}
+
+fun foo1(){
+    if (x < 0){
+        println("Helloo")
+    }
+}
+
+fun foo2(){
+    if (x<0) { println("Hello")
+    }
+}
+
+fun foo3(){
+    if (x == 5){
+        println(5)
+    }
+    else if (x == 6)
+        println(6)
+    else
+        println(7)
+}
+
+fun foo4(){
+    if (x > 4){
+        println(4)
+    }else if (x < 4){
+        println(5)
+    }
+}
