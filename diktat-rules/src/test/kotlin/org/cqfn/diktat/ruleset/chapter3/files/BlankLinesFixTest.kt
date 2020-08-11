a704859c1b8966d3eb8db347e8b65388ef7043ab
Peter Trifanov <peter.trifanov@mail.ru>
2020-08-11 15:23:19 +0300

    Implementation of recommendation 3.6 - usage of blank lines
    ### What's done:
    * Fixes after merge with master

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/files/BlankLinesFixTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/files/BlankLinesFixTest.kt
@@ -2,7 +2,7 @@ package org.cqfn.diktat.ruleset.chapter3.files
 
 import org.cqfn.diktat.ruleset.rules.files.BlankLinesRule
 import org.cqfn.diktat.util.FixTestBase
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class BlankLinesFixTest : FixTestBase("test/paragraph3/blank_lines", BlankLinesRule()) {
     @Test
