9b042600fcc34894bba6f61f4062a77b91fd17b3
aktsay <aktsay6@gmail.com>
2020-08-18 15:51:17 +0300

    Recommendation-3.8-no-spaces
    ### What's done:
      White Space check
      Tests were made

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/NoSpacesRule.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/NoSpacesRule.kt
@@ -2,24 +2,22 @@ package org.cqfn.diktat.ruleset.rules
 
 import com.pinterest.ktlint.core.KtLint
 import com.pinterest.ktlint.core.Rule
-import com.pinterest.ktlint.core.ast.ElementType
-import com.pinterest.ktlint.core.ast.ElementType.CLASS
-import com.pinterest.ktlint.core.ast.ElementType.CLASS_KEYWORD
-import com.pinterest.ktlint.core.ast.ElementType.FUN
-import com.pinterest.ktlint.core.ast.ElementType.COLON
-import com.pinterest.ktlint.core.ast.ElementType.COMMA
-import com.pinterest.ktlint.core.ast.ElementType.PROPERTY
 import com.pinterest.ktlint.core.ast.ElementType.WHITE_SPACE
-import com.pinterest.ktlint.core.ast.ElementType.VALUE_PARAMETER_LIST
+import com.pinterest.ktlint.core.ast.isWhiteSpaceWithNewline
+import org.cqfn.diktat.common.config.rules.RuleConfiguration
 import org.cqfn.diktat.common.config.rules.RulesConfig
+import org.cqfn.diktat.common.config.rules.getRuleConfig
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.com.intellij.psi.tree.TokenSet
 import org.cqfn.diktat.ruleset.constants.Warnings.TOO_MANY_SPACES
-import org.cqfn.diktat.ruleset.constants.Warnings.NO_SPACE_COLON
-import org.cqfn.diktat.ruleset.constants.Warnings.NO_SPACE_COMMA
 import org.cqfn.diktat.ruleset.utils.*
 
 class NoSpacesRule : Rule("no-spaces") {
+
+    companion object {
+        private const val MAX_SPACES = 1L
+    }
+
+
     private lateinit var configRules: List<RulesConfig>
     private lateinit var emitWarn: ((offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit)
     private var fileName: String? = null
@@ -34,90 +32,28 @@ class NoSpacesRule : Rule("no-spaces") {
         emitWarn = emit
         isFixMode = autoCorrect
 
-        when(node.elementType) {
-            PROPERTY -> checkProperty(node)
-//            FUN -> checkFunction(node)
-//            CLASS -> checkClass(node)
-        }
+        val configuration = NoSpacesRuleConfiguration(
+                configRules.getRuleConfig(TOO_MANY_SPACES)?.configuration ?: mapOf())
 
-        //print(node.prettyPrint())
-    }
-
-
-
-
-    private fun checkProperty(node: ASTNode){
-        checkSpaces(node)
-
-        if (node.hasChildMatching { it.elementType == COLON }) {
-            node.findChildAfter(COLON, WHITE_SPACE) ?: NO_SPACE_COLON
-                    .warn(configRules, emitWarn, isFixMode, "no space after colon", node.startOffset)
-        }
-
-        //print(node.prettyPrint())
-    }
-
-    private fun checkClass(node: ASTNode) {
-        //print(node.prettyPrint(maxLevel = 1))
-
-        val list = node.getAllChildrenWithType(WHITE_SPACE)
-
-        if (node.findChildBefore(WHITE_SPACE, CLASS_KEYWORD) != null) {
-            list.forEach {
-                if (it.text.length > 1) {
-                    TOO_MANY_SPACES.warn(configRules, emitWarn, isFixMode, "${it.text.length}", node.startOffset)
-                }
-            }
-        }
-        else {
-            list.drop(1).forEach {
-                if (it.text.length > 1) {
-                    TOO_MANY_SPACES.warn(configRules, emitWarn, isFixMode, "${it.text.length}", node.startOffset)
-                }
-            }
-        }
-    }
-
-
-    private fun checkSpaces(node: ASTNode) {
-        node.getChildren(TokenSet.WHITE_SPACE).forEach {
-            val spaces = it.text.length
-            if(spaces > 1){
-                TOO_MANY_SPACES.warn(configRules, emitWarn, isFixMode, "$spaces", node.startOffset)
-            }
-        }
-    }
-
-    private fun checkFunction(node: ASTNode) {
-        node.getAllChildrenWithType(VALUE_PARAMETER_LIST).forEach { checkParams(it) }
-
-        node.getAllChildrenWithType(COLON).forEach {
-            if (it.treeNext.elementType != WHITE_SPACE) {
-                NO_SPACE_COLON.warn(configRules, emitWarn, isFixMode, "no space after colon", node.startOffset)
-            }
-            if (it.treePrev.elementType != WHITE_SPACE) {
-                NO_SPACE_COLON.warn(configRules, emitWarn, isFixMode, "no space before colon", node.startOffset)
-            }
+        if (node.elementType == WHITE_SPACE){
+            checkWhiteSpace(node, configuration)
         }
 
     }
 
-    private fun checkParams(node: ASTNode) {
-        node.getAllChildrenWithType(ElementType.VALUE_PARAMETER).forEach {
-            it.findChildAfter(COLON, WHITE_SPACE) ?: NO_SPACE_COLON
-                    .warn(configRules, emitWarn, isFixMode, "no space after colon", node.startOffset)
 
-            checkSpaces(it)
-        }
+    private fun checkWhiteSpace(node: ASTNode, configuration: NoSpacesRuleConfiguration) {
+        val space = node.textLength
 
-        //print(node.prettyPrint())
-
-        node.getAllChildrenWithType(COMMA).forEach {
-
-            if(it.treeNext.elementType != WHITE_SPACE) {
-                NO_SPACE_COMMA
-                        .warn(configRules, emitWarn, isFixMode, "no space after comma", node.startOffset)
-            }
+        if (space > configuration.numberOfSpaces && !node.isWhiteSpaceWithNewline()) {
+            TOO_MANY_SPACES.warn(configRules, emitWarn, isFixMode, "", node.startOffset)
         }
     }
+
+
+
+    class NoSpacesRuleConfiguration(config: Map<String, String>) : RuleConfiguration(config) {
+        val numberOfSpaces = config["spaces"]?.toLongOrNull() ?: MAX_SPACES
+    }
+
 }
--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/NoSpacesRuleWarn.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/NoSpacesRuleWarn.kt
@@ -1,35 +1,230 @@
 package org.cqfn.diktat.ruleset.chapter3
 
+import com.pinterest.ktlint.core.LintError
+import org.cqfn.diktat.common.config.rules.RulesConfig
+import org.cqfn.diktat.ruleset.constants.Warnings
 import org.cqfn.diktat.ruleset.rules.BracesInConditionalsAndLoopsRule
 import org.cqfn.diktat.ruleset.rules.NoSpacesRule
 import org.cqfn.diktat.util.FixTestBase
 import org.cqfn.diktat.util.lintMethod
+import org.cqfn.diktat.ruleset.constants.Warnings.TOO_MANY_SPACES
+import org.cqfn.diktat.ruleset.rules.DIKTAT_RULE_SET_ID
 import org.junit.jupiter.api.Test
 
 
 class NoSpacesRuleWarn {
+
+
+    private val ruleId = "$DIKTAT_RULE_SET_ID:no-spaces"
+
+
+    private val rulesConfigListNoSpaces: List<RulesConfig> = listOf(
+            RulesConfig(TOO_MANY_SPACES.name, true,
+                    mapOf("spaces" to "2"))
+    )
+
     @Test
-    fun `test`() {
+    fun `enum spaces check bad`() {
         lintMethod(NoSpacesRule(),
                 """
-                    |import com.pinterest.ktlint.core.KtLint; import com.pinterest.ktlint.core.LintError
-                    |
-                    |fun foo() : Boolean {
-                    |    val a: Int = 5
-                    |    if (x < -5) {
-                    |       goo(); hoo()
-                    |    }
-                    |    else {
-                    |    }
-                    |    
-                    |    when(x) {
-                    |       1 -> println(1)
-                    |       else -> println("3;5")
-                    |    }
-                    |    val a = 5; val b = 10
-                    |    println(1); println(1)
+                    |enum       class IntArithmetics : BinaryOperator<Int> {
+                    |    PLUS, ASD
+                    |}
+                """.trimMargin(),
+                LintError(1, 5, ruleId, "${TOO_MANY_SPACES.warnText()} ", false)
+        )
+    }
+
+    @Test
+    fun `enum spaces check good`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |enum class SomeEnum {
+                    |    PLUS
                     |}
                 """.trimMargin()
         )
     }
+
+
+    @Test
+    fun `fun space check good`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class A {
+                    |   fun testFunction(val a = 5) {
+                    |   }
+                    |}
+                """.trimMargin()
+        )
+    }
+
+    @Test
+    fun `fun space check bad`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class A {
+                    |   fun      testFunction(val a =     6) {
+                    |   }
+                    |}
+                """.trimMargin(),
+                LintError(2, 7 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false),
+                LintError(2, 33 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false)
+        )
+    }
+
+    @Test
+    fun `class space check bad`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class     SomeClass {
+                    |   inner     class InnerClass{
+                    |   }
+                    |}
+                """.trimMargin(),
+                LintError(1, 6 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false),
+                LintError(2, 9 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false)
+        )
+    }
+
+    @Test
+    fun `class space check good`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class SomeClass {
+                    |   inner class InnerClass{
+                    |   }
+                    |}
+                """.trimMargin()
+        )
+    }
+
+
+    @Test
+    fun `property space check good`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class SomeClass {
+                    |   fun someFunc() {
+                    |       val a = 5
+                    |       val b: Int = 3
+                    |       val c: Int
+                    |   }
+                    |}
+                """.trimMargin()
+        )
+    }
+
+    @Test
+    fun `property space check bad`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class SomeClass {
+                    |   fun someFunc() {
+                    |       val a =    5
+                    |       val b: Int     = 3
+                    |       val c:     Int
+                    |   }
+                    |}
+                """.trimMargin(),
+                LintError(3, 15 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false),
+                LintError(4, 18 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false),
+                LintError(5, 14 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false)
+
+        )
+    }
+
+
+    @Test
+    fun `generic space check good`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class Box<T>(t: T){
+                    |   var value = t
+                    |}
+                """.trimMargin()
+                )
+    }
+
+    @Test
+    fun `generic space check bad`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class Box<   T>(t:    T){
+                    |   var value = t
+                    |}
+                """.trimMargin(),
+                LintError(1, 11 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false),
+                LintError(1, 19 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false)
+        )
+    }
+
+    @Test
+    fun `interface space check good`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |interface TestInterface{
+                    |   fun foo()
+                    |   fun bar()
+                    |}
+                """.trimMargin()
+        )
+    }
+
+    @Test
+    fun `interface space check bad`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |interface       TestInterface{
+                    |   fun foo()
+                    |   fun bar()
+                    |}
+                """.trimMargin(),
+                LintError(1, 10 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false)
+        )
+    }
+
+
+    @Test
+    fun `init space check bad`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class SomeClass{
+                    |   init     {
+                    |       print("SomeThing")
+                    |   }
+                    |}
+                """.trimMargin(),
+                LintError(2, 8 , ruleId,"${TOO_MANY_SPACES.warnText()} ", false)
+        )
+    }
+
+
+    @Test
+    fun `init space check good`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class SomeClass{
+                    |   init {
+                    |       print("SomeThing")
+                    |   }
+                    |}
+                """.trimMargin()
+        )
+    }
+
+    @Test
+    fun `config check`() {
+        lintMethod(NoSpacesRule(),
+                """
+                    |class  SomeClass  {
+                    |   init  {
+                    |       print("SomeThing")
+                    |   }
+                    |}
+                """.trimMargin(),
+                rulesConfigList = rulesConfigListNoSpaces
+        )
+    }
+
 }
