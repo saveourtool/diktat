b2ccbe4e63f311db0932653eac39539aef2434eb
soWhoAmI <aktsay6@gmail.com>
2020-08-24 15:13:13 +0300

    Recommendation-3.8-no-spaces
    ### What's done:
      * Tags in tests are now as WarningNames

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/ConsecutiveSpacesRuleFixTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/ConsecutiveSpacesRuleFixTest.kt
@@ -1,5 +1,6 @@
 package org.cqfn.diktat.ruleset.chapter3
 
+import generated.WarningNames
 import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.cqfn.diktat.ruleset.constants.Warnings
 import org.cqfn.diktat.ruleset.rules.ConsecutiveSpacesRule
@@ -18,13 +19,13 @@ class ConsecutiveSpacesRuleFixTest : FixTestBase("test/paragraph3/spaces", Conse
         )
 ) {
     @Test
-    @Tag("TOO_MANY_SPACES")
+    @Tag(WarningNames.TOO_MANY_CONSECUTIVE_SPACES)
     fun `many spaces rule enum case`() {
         fixAndCompare("TooManySpacesEnumExpected.kt", "TooManySpacesEnumTest.kt")
     }
 
     @Test
-    @Tag("TOO_MANY_SPACES")
+    @Tag(WarningNames.TOO_MANY_CONSECUTIVE_SPACES)
     fun `many spaces rule`() {
         fixAndCompare("TooManySpacesExpected.kt", "TooManySpacesTest.kt")
     }
