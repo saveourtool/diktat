3fbde3207f3224234e333c85e85e277c4658adc5
Peter Trifanov <peter.trifanov@mail.ru>
2020-08-25 16:09:49 +0300

    Recommendation 3.7 parts 5 and 6 (#190)
    Implementation of recommendation 3.7 - horizontal spaces
    
    ### What's done:
    * Added inspections for comma, semicolon and colon
    * Added inspection for nullable types
    * pom cleanup
    * Reverted kotlin version to 1.3.72
    * Removed 'tests' column from available-rules.md

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/WhiteSpaceRule.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/WhiteSpaceRule.kt
@@ -5,8 +5,10 @@ import com.pinterest.ktlint.core.Rule
 import com.pinterest.ktlint.core.ast.ElementType.ARROW
 import com.pinterest.ktlint.core.ast.ElementType.BINARY_EXPRESSION
 import com.pinterest.ktlint.core.ast.ElementType.CATCH_KEYWORD
+import com.pinterest.ktlint.core.ast.ElementType.CLASS
 import com.pinterest.ktlint.core.ast.ElementType.COLON
 import com.pinterest.ktlint.core.ast.ElementType.COLONCOLON
+import com.pinterest.ktlint.core.ast.ElementType.COMMA
 import com.pinterest.ktlint.core.ast.ElementType.CONSTRUCTOR_KEYWORD
 import com.pinterest.ktlint.core.ast.ElementType.DOT
 import com.pinterest.ktlint.core.ast.ElementType.DO_KEYWORD
@@ -20,13 +22,21 @@ import com.pinterest.ktlint.core.ast.ElementType.INIT_KEYWORD
 import com.pinterest.ktlint.core.ast.ElementType.LAMBDA_EXPRESSION
 import com.pinterest.ktlint.core.ast.ElementType.LBRACE
 import com.pinterest.ktlint.core.ast.ElementType.LPAR
+import com.pinterest.ktlint.core.ast.ElementType.NULLABLE_TYPE
+import com.pinterest.ktlint.core.ast.ElementType.OBJECT_DECLARATION
 import com.pinterest.ktlint.core.ast.ElementType.OPERATION_REFERENCE
 import com.pinterest.ktlint.core.ast.ElementType.POSTFIX_EXPRESSION
+import com.pinterest.ktlint.core.ast.ElementType.PROPERTY
+import com.pinterest.ktlint.core.ast.ElementType.QUEST
 import com.pinterest.ktlint.core.ast.ElementType.RANGE
 import com.pinterest.ktlint.core.ast.ElementType.SAFE_ACCESS
+import com.pinterest.ktlint.core.ast.ElementType.SECONDARY_CONSTRUCTOR
+import com.pinterest.ktlint.core.ast.ElementType.SEMICOLON
 import com.pinterest.ktlint.core.ast.ElementType.TRY_KEYWORD
 import com.pinterest.ktlint.core.ast.ElementType.TYPE_CONSTRAINT
+import com.pinterest.ktlint.core.ast.ElementType.TYPE_PARAMETER
 import com.pinterest.ktlint.core.ast.ElementType.VALUE_ARGUMENT
+import com.pinterest.ktlint.core.ast.ElementType.VALUE_PARAMETER
 import com.pinterest.ktlint.core.ast.ElementType.WHEN_KEYWORD
 import com.pinterest.ktlint.core.ast.ElementType.WHILE_KEYWORD
 import com.pinterest.ktlint.core.ast.ElementType.WHITE_SPACE
@@ -34,16 +44,23 @@ import com.pinterest.ktlint.core.ast.nextCodeLeaf
 import com.pinterest.ktlint.core.ast.parent
 import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.cqfn.diktat.ruleset.constants.Warnings.WRONG_WHITESPACE
+import org.cqfn.diktat.ruleset.utils.log
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
 import org.jetbrains.kotlin.com.intellij.psi.tree.TokenSet
 import org.jetbrains.kotlin.psi.psiUtil.parents
+import org.jetbrains.kotlin.psi.psiUtil.parentsWithSelf
 
 /**
  * This rule checks usage of whitespaces for horizontal code separation
  * 1. There should be single space between keyword and (, unless keyword is `constructor`
  * 2. There should be single space between keyword and {
+ * 3. There should be single space before any {, unless lambda inside parentheses of argument list
+ * 4. Binary operators should be surrounded by whitespaces, excluding :: and .
+ * 5. Spaces should be used after `,`, `:`, `;` (except cases when those symbols are in the end of line).
+ *    There should be no whitespaces in the end of line.
+ * 6. There should be only one space between identifier and it's type, if type is nullable there should be no spaces before `?`
  */
 @Suppress("ForbiddenComment")
 class WhiteSpaceRule : Rule("horizontal-whitespace") {
@@ -79,7 +96,11 @@ class WhiteSpaceRule : Rule("horizontal-whitespace") {
             CONSTRUCTOR_KEYWORD -> handleConstructor(node)
             in keywordsWithSpaceAfter -> handleKeywordWithParOrBrace(node)
             LBRACE -> handleLbrace(node)
-            OPERATION_REFERENCE, COLON, COLONCOLON, DOT, ARROW, SAFE_ACCESS -> handleBinaryOperator(node)
+            OPERATION_REFERENCE, COLONCOLON, DOT, ARROW, SAFE_ACCESS -> handleBinaryOperator(node)
+            COMMA, SEMICOLON -> handleToken(node, 0, 1)
+            COLON -> handleColon(node)
+            QUEST -> if (node.treeParent.elementType == NULLABLE_TYPE) handleToken(node, 0, null)
+            WHITE_SPACE -> handleEolWhiteSpace(node)
         }
     }
 
@@ -97,8 +118,8 @@ class WhiteSpaceRule : Rule("horizontal-whitespace") {
         if (node.treeNext.numWhiteSpaces() != 1) {
             // there is either not single whitespace or newline after keyword
             val nextCodeLeaf = node.nextCodeLeaf()!!
-            if (nextCodeLeaf.elementType != LPAR && nextCodeLeaf.elementType != LBRACE) {
-                // keywords without code block, e.g. else. Fixme: should be handled too
+            if (nextCodeLeaf.elementType != LPAR && nextCodeLeaf.elementType != LBRACE && node.treeNext.textContains('\n')) {
+                // statement after keyword doesn't have braces and starts at new line, e.g. `if (condition) foo()\n else\n bar()`
                 return
             }
             WRONG_WHITESPACE.warnAndFix(configRules, emitWarn, isFixMode, "keyword '${node.text}' should be separated from " +
@@ -143,40 +164,72 @@ class WhiteSpaceRule : Rule("horizontal-whitespace") {
         }
     }
 
-    private fun handleBinaryOperator(node: ASTNode) {
-        if (node.elementType == COLON && node.treeParent.elementType != TYPE_CONSTRAINT) {
-            return
+    private fun handleColon(node: ASTNode) {
+        when (node.treeParent.elementType) {
+            CLASS, SECONDARY_CONSTRUCTOR, TYPE_CONSTRAINT, TYPE_PARAMETER, OBJECT_DECLARATION -> handleBinaryOperator(node)
+            VALUE_PARAMETER, PROPERTY -> handleToken(node, 0, 1)
+            // fixme: find examples or delete this line
+            else -> log.warn("Colon with treeParent.elementType=${node.treeParent.elementType}, not handled by WhiteSpaceRule")
         }
+    }
 
+    private fun handleBinaryOperator(node: ASTNode) {
         val operatorNode = if (node.elementType == OPERATION_REFERENCE) node.firstChildNode else node
 
         if (node.elementType == OPERATION_REFERENCE && node.treeParent.elementType.let { it == BINARY_EXPRESSION || it == POSTFIX_EXPRESSION } ||
                 node.elementType != OPERATION_REFERENCE) {
-            checkAndFixBinaryOperator(node, operatorNode)
+            val requiredNumSpaces = if (operatorNode.elementType in operatorsWithNoWhitespace) 0 else 1
+            handleToken(node, requiredNumSpaces, requiredNumSpaces)
         }
     }
 
-    private fun checkAndFixBinaryOperator(node: ASTNode, operatorNode: ASTNode) {
+    private fun handleToken(node: ASTNode, requiredSpacesBefore: Int?, requiredSpacesAfter: Int?) {
+        require(requiredSpacesBefore != null || requiredSpacesAfter != null)
         val spacesBefore = node.treePrev.numWhiteSpaces()
-        val spacesAfter = (node.treeNext ?: node.treeParent.treeNext)  // for `!!` and possibly other postfix expressions treeNext can be null
-                .numWhiteSpaces()
-        val requiredNumSpaces = if (operatorNode.elementType in operatorsWithNoWhitespace) 0 else 1
-        if (spacesBefore != null && spacesBefore != requiredNumSpaces || spacesAfter != null && spacesAfter != requiredNumSpaces) {
-            val freeText = "${node.text} should ${if (requiredNumSpaces == 0) "not " else ""}be surrounded by whitespaces"
+        val spacesAfter = requiredSpacesAfter?.let { _ ->
+            (node.treeNext
+                    ?: node.treeParent.treeNext)  // for `!!` and possibly other postfix expressions treeNext can be null
+                    .numWhiteSpaces()
+        }
+        val isErrorBefore = requiredSpacesBefore != null && spacesBefore != null && spacesBefore != requiredSpacesBefore
+        val isErrorAfter = requiredSpacesAfter != null && spacesAfter != null && spacesAfter != requiredSpacesAfter
+        if (isErrorBefore || isErrorAfter) {
+            val freeText = "${node.text} should have" +
+                    getDescription(requiredSpacesBefore != null, requiredSpacesAfter != null, requiredSpacesBefore, requiredSpacesAfter) +
+                    ", but has" +
+                    getDescription(isErrorBefore, isErrorAfter, spacesBefore, spacesAfter)
             WRONG_WHITESPACE.warnAndFix(configRules, emitWarn, isFixMode, freeText, node.startOffset) {
-                node.fixSpaceAround(requiredNumSpaces)
+                node.fixSpaceAround(requiredSpacesBefore, requiredSpacesAfter)
             }
         }
     }
 
-    private fun ASTNode.fixSpaceAround(requiredNumSpaces: Int) {
-        if (requiredNumSpaces == 1) {
-            treePrev.let { if (it.elementType == WHITE_SPACE) it.treePrev else it }.leaveSingleWhiteSpace()
-            leaveSingleWhiteSpace()
-        } else if (requiredNumSpaces == 0) {
-            treePrev.removeIfWhiteSpace()
-            // for `!!` and possibly other postfix expressions treeNext can be null
-            (treeNext ?: treeParent.treeNext).removeIfWhiteSpace()
+    private fun handleEolWhiteSpace(node: ASTNode) {
+        val hasSpaces = node.text.substringBefore('\n').contains(' ')
+        // the second condition corresponds to the last line of file
+        val isEol = node.textContains('\n') || node.psi.parentsWithSelf.all { it.nextSibling == null }
+        if (hasSpaces && isEol) {
+            WRONG_WHITESPACE.warnAndFix(configRules, emitWarn, isFixMode, "there should be no spaces in the end of line", node.startOffset) {
+                (node as LeafElement).replaceWithText(node.text.trimStart(' '))
+            }
+        }
+    }
+
+    private fun ASTNode.fixSpaceAround(requiredSpacesBefore: Int?, requiredSpacesAfter: Int?) {
+        if (requiredSpacesBefore != null) {
+            if (requiredSpacesBefore == 1) {
+                (if (treePrev.elementType == WHITE_SPACE) treePrev.treePrev else treePrev).leaveSingleWhiteSpace()
+            } else if (requiredSpacesBefore == 0) {
+                treePrev.removeIfWhiteSpace()
+            }
+        }
+        if (requiredSpacesAfter != null) {
+            if (requiredSpacesAfter == 1) {
+                leaveSingleWhiteSpace()
+            } else if (requiredSpacesAfter == 0) {
+                // for `!!` and possibly other postfix expressions treeNext can be null
+                (treeNext ?: treeParent.treeNext).removeIfWhiteSpace()
+            }
         }
     }
 
@@ -200,4 +253,18 @@ class WhiteSpaceRule : Rule("horizontal-whitespace") {
 
     private fun ASTNode.removeIfWhiteSpace() = takeIf { it.elementType == WHITE_SPACE && !it.textContains('\n') }
             ?.let { it.treeParent.removeChild(it) }
+
+    private fun getDescription(shouldBefore: Boolean,
+                               shouldAfter: Boolean,
+                               before: Int?,
+                               after: Int?): String =
+            if (shouldBefore && shouldAfter) {
+                " $before space(s) before and $after space(s) after"
+            } else if (shouldBefore && !shouldAfter) {
+                " $before space(s) before"
+            } else if (shouldAfter) {
+                " $after space(s) after"
+            } else {
+                ""
+            }
 }
