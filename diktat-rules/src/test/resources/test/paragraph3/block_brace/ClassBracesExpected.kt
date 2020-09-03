a3219cbab0deb14ad43822395e63580e9c670c17
kentr0w <qbextted0@gmail.com>
2020-09-03 14:29:48 +0300

    False-positive activation of 1TBS rule (#221)
    * False-positive activation of 1TBS rule
    
    ### What's done:
      Added check for lambdas
      Removed handle for space before open brace

--- diktat-rules/src/test/resources/test/paragraph3/block_brace/ClassBracesExpected.kt
+++ diktat-rules/src/test/resources/test/paragraph3/block_brace/ClassBracesExpected.kt
@@ -1,15 +1,15 @@
 package test.paragraph3.block_brace
 
-class A {
+class A{
     private val id = 10
 }
 
-class B {
+class B{
     private val id = 10
 }
 
-class C {
-    companion object {
+class C{
+    companion object{
         private val id = 11
 }
 }
