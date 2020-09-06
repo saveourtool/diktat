5e4f5b8870241cfef677cb9aafcb7e9a4ae55f94
soWhoAmI <aktsay6@gmail.com>
2020-09-06 13:20:06 +0300

    feature/recommendation-3.13-long-numerical-values-separated ### What's done:   * Added new values to fix test

--- diktat-rules/src/test/resources/test/paragraph3/long_numbers/LongNumericalValuesTest.kt
+++ diktat-rules/src/test/resources/test/paragraph3/long_numbers/LongNumericalValuesTest.kt
@@ -8,4 +8,7 @@ fun foo() {
     val hexBytes = 0xFFECDE5E
     val bytes = 0b11010010011010011001010010010010
     val flo = 192.312341341344355345
+    val flo2 = 192.111_111_1
+    val flo3 = 1924345.145f
+    val flo4 = 1924345.14511111
 }
