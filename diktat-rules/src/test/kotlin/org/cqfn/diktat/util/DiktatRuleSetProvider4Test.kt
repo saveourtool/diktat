bd8484e10e8d70b55aacd58cfd9efcea2dd776cd
kentr0w <qbextted0@gmail.com>
2020-09-08 00:08:46 +0300

    Add all rule's in DiktatRuleSetProvider
    ### What's done:
        Added test for check it

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/util/DiktatRuleSetProvider4Test.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/util/DiktatRuleSetProvider4Test.kt
@@ -5,18 +5,46 @@ import com.pinterest.ktlint.core.RuleSet
 import com.pinterest.ktlint.core.RuleSetProvider
 import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.cqfn.diktat.common.config.rules.RulesConfigReader
+import org.cqfn.diktat.ruleset.rules.DiktatRuleSetProvider
 import org.cqfn.diktat.ruleset.rules.RuleSetDiktat
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import java.io.File
+import java.net.URI
+import java.nio.file.Path
+import java.nio.file.Paths
 
 /**
  * simple class for emulating RuleSetProvider to inject .json rule configuration and mock this part of code
  */
 class DiktatRuleSetProvider4Test(val rule: Rule, rulesConfigList: List<RulesConfig>?) : RuleSetProvider {
-    private val rulesConfigList: List<RulesConfig>? = rulesConfigList ?: RulesConfigReader(javaClass.classLoader).readResource("rules-config.json")
+    private val rulesConfigList: List<RulesConfig>? = rulesConfigList
+            ?: RulesConfigReader(javaClass.classLoader).readResource("rules-config.json")
 
     override fun get(): RuleSet {
         return RuleSetDiktat(
-                rulesConfigList?: listOf(),
+                rulesConfigList ?: listOf(),
                 rule
         )
+    }
+}
+
+class DiktatRuleSetProviderTest {
+
+    companion object {
+        private val IGNORE_FILE = listOf("DiktatRuleSetProvider")
+    }
+
+    @Test
+    fun `check DiktatRuleSetProviderTest contain all rules`() {
+        val path = "${System.getProperty("user.dir")}/src/main/kotlin/org/cqfn/diktat/ruleset/rules"
+        val rulesName = File(path).walk().filter { it.isFile }.map { it.nameWithoutExtension }.toMutableList()
+        rulesName.removeAll(IGNORE_FILE)
+        val q = DiktatRuleSetProvider().get()
+                .map { it.toString().split('.').last() }
+                .map { it.substring(0, it.indexOf('@')) }
+                .toMutableList()
+        Assertions.assertEquals(rulesName.sort(), q.sort())
     }
 }
