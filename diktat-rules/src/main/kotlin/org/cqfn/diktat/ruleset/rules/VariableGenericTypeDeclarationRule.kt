69b6f80c5291e2108b9617bebbdcdc807b7fd831
soWhoAmI <aktsay6@gmail.com>
2020-09-24 15:52:26 +0300

    feature/recommendation-4.3.2-generic-explicit-declaration ### What's done:   * Fixed bugs   * Added fixme

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/VariableGenericTypeDeclarationRule.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/VariableGenericTypeDeclarationRule.kt
@@ -10,6 +10,12 @@ import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.cqfn.diktat.ruleset.constants.Warnings
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 
+/**
+ * This Rule checks if declaration of a generic variable is appropriate.
+ * Not recommended: val myVariable: Map<Int, String> = emptyMap<Int, String>() or val myVariable = emptyMap<Int, String>()
+ * Recommended: val myVariable: Map<Int, String> = emptyMap()
+ */
+// FIXME: we now don't have access to return types, so we can perform this check only if explicit type is present, but should be able also if it's not.
 class VariableGenericTypeDeclarationRule(private val configRules: List<RulesConfig>) : Rule("variable-generic-type") {
     private lateinit var emitWarn: ((offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit)
     private var isFixMode: Boolean = false
@@ -19,10 +25,6 @@ class VariableGenericTypeDeclarationRule(private val configRules: List<RulesConf
                        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit) {
         emitWarn = emit
         isFixMode = autoCorrect
-
-        val lazyValue: List<Int> by lazy {
-            listOf<Int>(1,2,3)
-        }
 
         when(node.elementType) {
             PROPERTY, VALUE_PARAMETER -> handleProperty(node)
@@ -38,7 +40,7 @@ class VariableGenericTypeDeclarationRule(private val configRules: List<RulesConf
         val leftSide = Regex("<([a-zA-Z, <>?]*)>").find(node.findChildByType(TYPE_REFERENCE)?.text ?: "")
 
         if ((rightSide != null && leftSide != null) && rightSide.groupValues.first() == leftSide.groupValues.first()) {
-            Warnings.GENERIC_VARIABLE_WRONG_DECLARATION.warnAndFix(configRules, emitWarn, isFixMode, node.text, node.startOffset, node) {
+            Warnings.GENERIC_VARIABLE_WRONG_DECLARATION.warnAndFix(configRules, emitWarn, isFixMode, "type arguments are unnecessary in $rightSide", node.startOffset, node) {
                 callExpr!!.removeChild(callExpr.findChildByType(TYPE_ARGUMENT_LIST)!!)
             }
         }
