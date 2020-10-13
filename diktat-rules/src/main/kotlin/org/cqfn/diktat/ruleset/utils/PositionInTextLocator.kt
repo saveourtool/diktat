9ff408054be566d936d6e7d7ba8b99b2f6437f0a
kentr0w <qbextted0@gmail.com>
2020-10-13 13:03:50 +0300

    Remove deprecated calculateLineColByOffset methods from Ktlint
    ### What's done:
        Renamed variables and fixed bugs

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/utils/PositionInTextLocator.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/utils/PositionInTextLocator.kt
@@ -5,36 +5,32 @@ internal typealias LineAndColumn = Pair<Int, Int>
 /**
  * Calculate position in text - line and column based on offset from the text start.
  */
-internal fun buildPositionInTextLocator(
-        text: String
-): (offset: Int) -> LineAndColumn {
+internal fun buildPositionInTextLocator(text: String): (offset: Int) -> LineAndColumn {
     val textLength = text.length
-    val arr = ArrayList<Int>()
-
+    val identifierArray = ArrayList<Int>()
     var endOfLineIndex = -1
+
     do {
-        arr.add(endOfLineIndex + 1)
+        identifierArray.add(endOfLineIndex + 1)
         endOfLineIndex = text.indexOf('\n', endOfLineIndex + 1)
     } while (endOfLineIndex != -1)
 
-    arr.add(textLength + if (arr.last() == textLength) 1 else 0)
+    identifierArray.add(textLength + if (identifierArray.last() == textLength) 1 else 0)
 
-    val segmentTree = SegmentTree(arr.toTypedArray())
+    val segmentTree = SegmentTree(identifierArray.toIntArray())
 
     return { offset ->
         val line = segmentTree.indexOf(offset)
         if (line != -1) {
-            val col = offset - segmentTree.get(line).left
-            line + 1 to col + 1
+            val column = offset - segmentTree.get(line).left
+            line + 1 to column + 1
         } else {
             1 to 1
         }
     }
 }
 
-private class SegmentTree(
-        sortedArray: Array<Int>
-) {
+private class SegmentTree(sortedArray: IntArray) {
 
     init {
         require(sortedArray.size > 1) { "At least two data points are required" }
@@ -50,22 +46,19 @@ private class SegmentTree(
                 Segment(element, sortedArray[index + 1] - 1)
             }
 
-    fun get(i: Int): Segment = segments[i]
-    fun indexOf(v: Int): Int = binarySearch(v, 0, segments.size - 1)
+    fun get(index: Int): Segment = segments[index]
 
-    private fun binarySearch(
-            v: Int,
-            l: Int,
-            r: Int
-    ): Int = when {
-        l > r -> -1
+    fun indexOf(index: Int): Int = binarySearch(index, 0, segments.size - 1)
+
+    private fun binarySearch(compareElement: Int, left: Int, right: Int): Int = when {
+        left > right -> -1
         else -> {
-            val i = l + (r - l) / 2
-            val s = segments[i]
-            if (v < s.left) {
-                binarySearch(v, l, i - 1)
+            val index = left + (right - left) / 2
+            val midElement = segments[index]
+            if (compareElement < midElement.left) {
+                binarySearch(compareElement, left, index - 1)
             } else {
-                if (s.right < v) binarySearch(v, i + 1, r) else i
+                if (midElement.right < compareElement) binarySearch(compareElement, index + 1, right) else index
             }
         }
     }
