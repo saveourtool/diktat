2180e7713d1851115701425a2069ae7ff6438711
Peter Trifanov <peter.trifanov@mail.ru>
2020-10-19 16:35:46 +0300

    Fixing bug with lineNumber in autocorrect mode (#400)
    ### What's done:
    * Use getLineNumber everywhere
    * Added test

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/identifiers/LocalVariablesRule.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/identifiers/LocalVariablesRule.kt
@@ -3,11 +3,11 @@ package org.cqfn.diktat.ruleset.rules.identifiers
 import com.pinterest.ktlint.core.Rule
 import com.pinterest.ktlint.core.ast.ElementType.FILE
 import com.pinterest.ktlint.core.ast.isPartOfComment
-import com.pinterest.ktlint.core.ast.lineNumber
 import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.cqfn.diktat.ruleset.constants.Warnings.LOCAL_VARIABLE_EARLY_DECLARATION
 import org.cqfn.diktat.ruleset.utils.containsOnlyConstants
 import org.cqfn.diktat.ruleset.utils.getDeclarationScope
+import org.cqfn.diktat.ruleset.utils.getLineNumber
 import org.cqfn.diktat.ruleset.utils.lastLineNumber
 import org.cqfn.diktat.ruleset.utils.search.findAllVariablesWithUsages
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
@@ -94,19 +94,19 @@ class LocalVariablesRule(private val configRules: List<RulesConfig>) : Rule("loc
     private fun handleLocalProperty(property: KtProperty, usages: List<KtNameReferenceExpression>) {
         val declarationScope = property.getDeclarationScope()
 
-        val firstUsageStatementLine = getFirstUsageStatementOrBlock(usages, declarationScope).node.lineNumber()!!
-        val firstUsage = usages.minBy { it.node.lineNumber()!! }!!
-        checkLineNumbers(property, firstUsageStatementLine, firstUsageLine = firstUsage.node.lineNumber()!!)
+        val firstUsageStatementLine = getFirstUsageStatementOrBlock(usages, declarationScope).node.getLineNumber(isFixMode)!!
+        val firstUsage = usages.minBy { it.node.getLineNumber(isFixMode)!! }!!
+        checkLineNumbers(property, firstUsageStatementLine, firstUsageLine = firstUsage.node.getLineNumber(isFixMode)!!)
     }
 
     @Suppress("UnsafeCallOnNullableType")
     private fun handleConsecutiveDeclarations(statement: PsiElement, properties: List<KtProperty>) {
         // need to check that properties are declared consecutively with only maybe empty lines
         properties
-                .sortedBy { it.node.lineNumber()!! }
+                .sortedBy { it.node.getLineNumber(isFixMode)!! }
                 .zip(properties.size - 1 downTo 0)
                 .forEach { (property, offset) ->
-                    checkLineNumbers(property, statement.node.lineNumber()!!, offset)
+                    checkLineNumbers(property, statement.node.getLineNumber(isFixMode)!!, offset)
                 }
     }
 
@@ -115,11 +115,11 @@ class LocalVariablesRule(private val configRules: List<RulesConfig>) : Rule("loc
         val numLinesToSkip = property
                 .siblings(forward = true, withItself = false)
                 .takeWhile { it is PsiWhiteSpace || it.node.isPartOfComment() }
-                .let { siblings -> siblings.last().node.lastLineNumber()!! - siblings.first().node.lineNumber()!! - 1 }
+                .let { siblings -> siblings.last().node.lastLineNumber(isFixMode)!! - siblings.first().node.getLineNumber(isFixMode)!! - 1 }
 
-        if (firstUsageStatementLine - numLinesToSkip != property.node.lastLineNumber()!! + 1 + offset) {
+        if (firstUsageStatementLine - numLinesToSkip != property.node.lastLineNumber(isFixMode)!! + 1 + offset) {
             LOCAL_VARIABLE_EARLY_DECLARATION.warn(configRules, emitWarn, isFixMode,
-                    warnMessage(property.name!!, property.node.lineNumber()!!, firstUsageLine
+                    warnMessage(property.name!!, property.node.getLineNumber(isFixMode)!!, firstUsageLine
                             ?: firstUsageStatementLine), property.startOffset, property.node)
         }
     }
@@ -130,7 +130,7 @@ class LocalVariablesRule(private val configRules: List<RulesConfig>) : Rule("loc
      */
     @Suppress("UnsafeCallOnNullableType")
     private fun getFirstUsageStatementOrBlock(usages: List<KtNameReferenceExpression>, declarationScope: KtBlockExpression?): PsiElement {
-        val firstUsage = usages.minBy { it.node.lineNumber()!! }!!
+        val firstUsage = usages.minBy { it.node.getLineNumber(isFixMode)!! }!!
         val firstUsageScope = firstUsage.getParentOfType<KtBlockExpression>(true)
 
         return if (firstUsageScope == declarationScope) {
@@ -140,7 +140,7 @@ class LocalVariablesRule(private val configRules: List<RulesConfig>) : Rule("loc
                     .find { it.parent == declarationScope }!!
         } else {
             // first usage is in deeper block compared to declaration, need to check how close is declaration to the first line of the block
-            usages.minBy { it.node.lineNumber()!! }!!
+            usages.minBy { it.node.getLineNumber(isFixMode)!! }!!
                     .parentsWithSelf
                     .find { it.parent == declarationScope }!!
         }
