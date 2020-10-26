f28dfb98110f7f890fc483f41287059272448221
Alexander Tsay <48321920+aktsay6@users.noreply.github.com>
2020-10-26 18:13:52 +0300

    Rule 6.1.6 abstract classes should have at least one abstract method (#457)
    rule-6.1.6-abstract-classes(#442)
    
    ### What's done:
      * Rule logic made
      * Added fix and warn tests

--- diktat-rules/src/main/kotlin/generated/WarningNames.kt
+++ diktat-rules/src/main/kotlin/generated/WarningNames.kt
@@ -196,4 +196,6 @@ public object WarningNames {
 
     public const val WRONG_NAME_OF_VARIABLE_INSIDE_ACCESSOR: String =
             "WRONG_NAME_OF_VARIABLE_INSIDE_ACCESSOR"
+
+    public const val CLASS_SHOULD_NOT_BE_ABSTRACT: String = "CLASS_SHOULD_NOT_BE_ABSTRACT"
 }
