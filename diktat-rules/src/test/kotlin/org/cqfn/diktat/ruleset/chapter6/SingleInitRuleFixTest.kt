75b973a7617555162a80511a41db30ff2141099f
Peter Trifanov <peter.trifanov@mail.ru>
2020-10-27 12:45:01 +0300

    Rule 6.1.4
    ### What's done:
    * More logic

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter6/SingleInitRuleFixTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter6/SingleInitRuleFixTest.kt
@@ -12,4 +12,16 @@ class SingleInitRuleFixTest: FixTestBase("test/chapter6/init_blocks", ::SingleIn
     fun `should merge init blocks`() {
         fixAndCompare("InitBlocksExpected.kt", "InitBlocksTest.kt")
     }
+
+    @Test
+    @Tag(WarningNames.MULTIPLE_INIT_BLOCKS)
+    fun `should move property assignments from init blocks to declarations`() {
+        fixAndCompare("InitBlockWithAssignmentsExpected.kt", "InitBlockWithAssignmentsTest.kt")
+    }
+
+    @Test
+    @Tag(WarningNames.MULTIPLE_INIT_BLOCKS)
+    fun `should merge init blocks and move property assignments from init blocks to declarations`() {
+        fixAndCompare("InitBlocksWithAssignmentsExpected.kt", "InitBlocksWithAssignmentsTest.kt")
+    }
 }
