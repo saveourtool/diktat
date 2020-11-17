54ad467de1b5dd005c177e7d1a5518f9012f3cc6
aktsay6 <aktsay6@gmail.com>
2020-11-17 11:06:26 +0300

    bugfix/false-positive-local-variable-early-declaration(#488)
    ### What's done:
      * Fixed bugs

--- diktat-rules/src/test/resources/test/funcTest/FunctionalTestFile.kt
+++ diktat-rules/src/test/resources/test/funcTest/FunctionalTestFile.kt
@@ -2,14 +2,14 @@ package test.funcTest
 
 class FunctionalTestFile {
     val AAAA = 5
+}
 
-    fun `method name incorrect, part 4`() {
-        val code = """
+fun `method name incorrect, part 4`() {
+    val code = """
                   class TestPackageName {
                     fun methODTREE(): String {
                     }
                   }
                 """.trimIndent()
-        lintMethod(code, LintError(2, 7, ruleId, "${FUNCTION_NAME_INCORRECT_CASE.warnText()} methODTREE", true))
-    }
+    lintMethod(code, LintError(2, 7, ruleId, "${FUNCTION_NAME_INCORRECT_CASE.warnText()} methODTREE", true))
 }
