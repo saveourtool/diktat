d8651d2009879e17167ec2c370e0d5023835f482
Denis Kumar <qbextted0@gmail.com>
2020-11-23 19:07:36 +0300

    Rule 6.1.3 Do not use the primary constructor if it is empty and has no sense (#528)
    * Rule 6.1.3
    
    ### What's done:
       Made rule, added tests and documentations

---
+++ diktat-rules/src/test/resources/test/chapter6/primary_constructor/EmptyPCTest.kt
@@ -0,0 +1,18 @@
+package test.chapter6.primary_constructor
+
+class Test() {
+    var a: Int = 0
+    var b: Int = 0
+}
+
+class Test() {
+    var a  = "Property"
+
+    init {
+        println("some init")
+    }
+
+    constructor(a: String): this() {
+        this.a = a
+    }
+}
