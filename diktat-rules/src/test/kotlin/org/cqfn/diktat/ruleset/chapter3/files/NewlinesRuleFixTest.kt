66bfec3e219b6f627162ff0a960501497d80b1ac
kentr0w <qbextted0@gmail.com>
2020-12-04 20:49:48 +0300

    functional style in NewlinesRule
    ### What's done:
       Fixed fix test

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/files/NewlinesRuleFixTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/files/NewlinesRuleFixTest.kt
@@ -4,10 +4,17 @@ import org.cqfn.diktat.ruleset.rules.files.NewlinesRule
 import org.cqfn.diktat.util.FixTestBase
 
 import generated.WarningNames
+import org.cqfn.diktat.common.config.rules.RulesConfig
+import org.cqfn.diktat.ruleset.constants.Warnings
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
 class NewlinesRuleFixTest : FixTestBase("test/paragraph3/newlines", ::NewlinesRule) {
+    private val rulesConfigListShort: List<RulesConfig> = listOf(
+            RulesConfig(Warnings.WRONG_NEWLINES.name, true,
+                    mapOf("maxCallsInOneLine" to "1"))
+    )
+
     @Test
     @Tag(WarningNames.REDUNDANT_SEMICOLON)
     fun `should remove redundant semicolons`() {
@@ -23,7 +30,7 @@ class NewlinesRuleFixTest : FixTestBase("test/paragraph3/newlines", ::NewlinesRu
     @Test
     @Tag(WarningNames.WRONG_NEWLINES)
     fun `should fix newlines to follow functional style`() {
-        fixAndCompare("FunctionalStyleExpected.kt", "FunctionalStyleTest.kt")
+        fixAndCompare("FunctionalStyleExpected.kt", "FunctionalStyleTest.kt", rulesConfigListShort )
     }
 
     @Test
