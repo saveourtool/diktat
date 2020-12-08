0bd31006f4f1a31608e2e93f22da2bd2753c9359
soWhoAmI <aktsay6@gmail.com>
2020-12-08 12:49:32 +0300

    bugfix/compact-incorrect-tree(#599)
    ### What's done:
      * Fixed bugs

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/classes/CompactInitialization.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/classes/CompactInitialization.kt
@@ -1,5 +1,9 @@
 package org.cqfn.diktat.ruleset.rules.classes
 
+import com.pinterest.ktlint.core.Rule
+import com.pinterest.ktlint.core.ast.isPartOfComment
+import com.pinterest.ktlint.core.ast.ElementType.LBRACE
+import com.pinterest.ktlint.core.ast.ElementType.WHITE_SPACE
 import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.cqfn.diktat.ruleset.constants.EmitType
 import org.cqfn.diktat.ruleset.constants.Warnings
@@ -16,10 +20,6 @@ import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.psiUtil.siblings
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
-import com.pinterest.ktlint.core.Rule
-import com.pinterest.ktlint.core.ast.ElementType.LBRACE
-import com.pinterest.ktlint.core.ast.ElementType.WHITE_SPACE
-import com.pinterest.ktlint.core.ast.isPartOfComment
 
 /**
  * This rules checks if an object initialization can be wrapped into an `apply` function.
