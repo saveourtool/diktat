ce7c794ec0b83a28ca677b44aadeaee292dc0ffa
soWhoAmI <aktsay6@gmail.com>
2020-12-08 12:06:56 +0300

    bugfix/compact-incorrect-tree(#599)
    ### What's done:
      * Fixed bugs

--- diktat-rules/src/test/resources/test/smoke/src/main/kotlin/Example4Test.kt
+++ diktat-rules/src/test/resources/test/smoke/src/main/kotlin/Example4Test.kt
@@ -1,5 +1,47 @@
 package org.cqfn.diktat
 
+class SpecialTagsInKdoc {
+
+    /**
+     * Empty function to test KDocs
+     * @apiNote foo
+     * @implSpec bar
+     *
+     *
+     * @implNote baz
+     */
+    fun test() = Unit
+}
+
+fun `method name incorrect, part 4`() {
+    val code = """
+                  class TestPackageName {
+                    fun methODTREE(): String {
+                    }
+                  }
+                """.trimIndent()
+    lintMethod(code, LintError(2, 7, ruleId, "${FUNCTION_NAME_INCORRECT_CASE.warnText()} methODTREE", true))
+
+    foo
+            // we are calling bar
+            .bar()
+
+    bar
+            /* This is a block comment */
+            .foo()
+}
+
+fun foo() {
+    foo(
+            0,
+            { obj -> obj.bar() }
+    )
+
+    bar(
+            0, { obj -> obj.bar() }
+    )
+}
+
 fun bar() {
     val diktatExtension = project.extensions.create(DIKTAT_EXTENSION, DiktatExtension::class.java)
     diktatExtension.inputs = project.fileTree("src").apply {
@@ -7,4 +49,3 @@ fun bar() {
     }
     diktatExtension.reporter = PlainReporter(System.out)
 }
-
