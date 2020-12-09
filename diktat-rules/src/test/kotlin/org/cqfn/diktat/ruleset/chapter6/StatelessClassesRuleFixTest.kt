8af5207f0456ac798f554d9ae9056413c2d5598a
aktsay <aktsay6@gmail.com>
2020-12-09 13:07:12 +0300

    feature/recommendation-6.4.2(#449)
    ### What's done:
     * Fixed bugs

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter6/StatelessClassesRuleFixTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter6/StatelessClassesRuleFixTest.kt
@@ -7,8 +7,7 @@ import generated.WarningNames.OBJECT_IS_PREFERRED
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
-class StatelessClassesRuleFixTest: FixTestBase("test/chapter6/stateless_classes", ::StatelessClassesRule) {
-
+class StatelessClassesRuleFixTest : FixTestBase("test/chapter6/stateless_classes", ::StatelessClassesRule) {
     @Test
     @Tag(OBJECT_IS_PREFERRED)
     fun `fix class to object keyword`() {
--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter6/StatelessClassesRuleWarnTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter6/StatelessClassesRuleWarnTest.kt
@@ -1,44 +1,46 @@
 package org.cqfn.diktat.ruleset.chapter6
 
-import com.pinterest.ktlint.core.LintError
-import generated.WarningNames.OBJECT_IS_PREFERRED
+
 import org.cqfn.diktat.ruleset.constants.Warnings
 import org.cqfn.diktat.ruleset.rules.DIKTAT_RULE_SET_ID
 import org.cqfn.diktat.ruleset.rules.classes.StatelessClassesRule
 import org.cqfn.diktat.util.LintTestBase
+
+import generated.WarningNames.OBJECT_IS_PREFERRED
+import com.pinterest.ktlint.core.LintError
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
-class StatelessClassesRuleWarnTest: LintTestBase(::StatelessClassesRule) {
+class StatelessClassesRuleWarnTest : LintTestBase(::StatelessClassesRule) {
+
     private val ruleId = "$DIKTAT_RULE_SET_ID:stateless-class"
 
     @Test
     @Tag(OBJECT_IS_PREFERRED)
     fun `should not trigger on class not extending any interface`() {
         lintMethod(
-                """
-                |class Some : I() {
-                |   override fun some()
-                |}
+            """
+            |class Some : I() {
+            |   override fun some()
+            |}
             """.trimMargin()
         )
     }
-
 
     @Test
     @Tag(OBJECT_IS_PREFERRED)
     fun `should trigger on class extending interface`() {
         lintMethod(
-                """
-                |class Some : I {
-                |   override fun some()
-                |}
-                |
-                |interface I {
-                |   fun some()
-                |}
+            """
+            |class Some : I {
+            |   override fun some()
+            |}
+            |
+            |interface I {
+            |   fun some()
+            |}
             """.trimMargin(),
-                LintError(1, 1, ruleId, "${Warnings.OBJECT_IS_PREFERRED.warnText()} class Some", true)
+            LintError(1, 1, ruleId, "${Warnings.OBJECT_IS_PREFERRED.warnText()} class Some", true)
         )
     }
 
@@ -46,11 +48,11 @@ class StatelessClassesRuleWarnTest: LintTestBase(::StatelessClassesRule) {
     @Tag(OBJECT_IS_PREFERRED)
     fun `should not trigger on class with constructor`() {
         lintMethod(
-                """
-                |class Some(b: Int) : I {
-                |   
-                |   override fun some()
-                |}
+            """
+            |class Some(b: Int) : I {
+            |   
+            |   override fun some()
+            |}
             """.trimMargin()
         )
     }
@@ -59,11 +61,11 @@ class StatelessClassesRuleWarnTest: LintTestBase(::StatelessClassesRule) {
     @Tag(OBJECT_IS_PREFERRED)
     fun `should not trigger on class with no interface in this file`() {
         lintMethod(
-                """
-                |class Some : I {
-                |   
-                |   override fun some()
-                |}
+            """
+            |class Some : I {
+            |   
+            |   override fun some()
+            |}
             """.trimMargin()
         )
     }
@@ -72,11 +74,11 @@ class StatelessClassesRuleWarnTest: LintTestBase(::StatelessClassesRule) {
     @Tag(OBJECT_IS_PREFERRED)
     fun `should not trigger on class with state`() {
         lintMethod(
-                """
-                |class Some : I {
-                |   val a = 5
-                |   override fun some()
-                |}
+            """
+            |class Some : I {
+            |   val a = 5
+            |   override fun some()
+            |}
             """.trimMargin()
         )
     }
