023275f823050dcb6f59be54e067d2cf52a96948
Peter Trifanov <peter.trifanov@mail.ru>
2020-12-10 12:32:58 +0300

    Fixing diktat-gradle-plugin
    ### What's done:
    * Improved functional tests

--- diktat-gradle-plugin/src/functionalTest/kotlin/org/cqfn/diktat/plugin/gradle/DiktatGradlePluginFunctionalTest.kt
+++ diktat-gradle-plugin/src/functionalTest/kotlin/org/cqfn/diktat/plugin/gradle/DiktatGradlePluginFunctionalTest.kt
@@ -12,14 +12,16 @@ import java.io.File
 
 class DiktatGradlePluginFunctionalTest {
     private val testProjectDir = TemporaryFolder()
+    private lateinit var buildFile: File
 
     @BeforeEach
     fun setUp() {
         testProjectDir.create()
         File("../examples/gradle-kotlin-dsl").copyRecursively(testProjectDir.root)
         File(testProjectDir.root, "build.gradle.kts").delete()
-        testProjectDir.newFile("build.gradle.kts").writeText(
-            """
+        buildFile = testProjectDir.newFile("build.gradle.kts").apply {
+            writeText(
+                """
                 plugins {
                     id("org.cqfn.diktat.diktat-gradle-plugin")
                 }
@@ -29,8 +31,8 @@ class DiktatGradlePluginFunctionalTest {
                     mavenCentral()
                 }
             """.trimIndent()
-        )
-
+            )
+        }
     }
 
     @AfterEach
@@ -40,33 +42,7 @@ class DiktatGradlePluginFunctionalTest {
 
     @Test
     fun `should execute diktatCheck on default values`() {
-        println(testProjectDir.root.listFiles().joinToString("\n"))
-        val result = GradleRunner.create()
-            .withProjectDir(testProjectDir.root)
-            .withArguments(DIKTAT_CHECK_TASK)
-            .withPluginClasspath()
-            .forwardOutput()
-            .runCatching {
-                buildAndFail()
-            }
-
-        require(result.isSuccess) { "Running gradle returned exception ${result.exceptionOrNull()}" }
-
-        val buildResult = result.getOrNull()!!
-        val diktatCheckBuildResult = buildResult.task(":$DIKTAT_CHECK_TASK")
-        requireNotNull(diktatCheckBuildResult)
-        Assertions.assertEquals(TaskOutcome.FAILED, diktatCheckBuildResult.outcome)
-        Assertions.assertTrue(
-            buildResult.output.contains("[HEADER_MISSING_OR_WRONG_COPYRIGHT]")
-        )
-    }
-
-/*    @Test
-    fun `should execute diktatCheck with explicit inputs`() {
-        val result = GradleRunner.create()
-            .withProjectDir(testProjectDir.root)
-            .withArguments(DIKTAT_CHECK_TASK)
-            .buildAndFail()
+        val result = runDiktat()
 
         val diktatCheckBuildResult = result.task(":$DIKTAT_CHECK_TASK")
         requireNotNull(diktatCheckBuildResult)
@@ -74,5 +50,37 @@ class DiktatGradlePluginFunctionalTest {
         Assertions.assertTrue(
             result.output.contains("[HEADER_MISSING_OR_WRONG_COPYRIGHT]")
         )
-    }*/
+    }
+
+    @Test
+    fun `should execute diktatCheck with explicit inputs`() {
+        buildFile.appendText(
+            """${System.lineSeparator()}
+                diktat {
+                    inputs = files("src/**/*.kt")
+                }
+            """.trimIndent()
+        )
+        val result = runDiktat()
+
+        val diktatCheckBuildResult = result.task(":$DIKTAT_CHECK_TASK")
+        requireNotNull(diktatCheckBuildResult)
+        Assertions.assertEquals(TaskOutcome.FAILED, diktatCheckBuildResult.outcome)
+        Assertions.assertTrue(
+            result.output.contains("[HEADER_MISSING_OR_WRONG_COPYRIGHT]")
+        )
+    }
+
+    private fun runDiktat() = GradleRunner.create()
+        .withProjectDir(testProjectDir.root)
+        .withArguments(DIKTAT_CHECK_TASK)
+        .withPluginClasspath()
+        .forwardOutput()
+        .runCatching {
+            buildAndFail()
+        }
+        .also {
+            require(it.isSuccess) { "Running gradle returned exception ${it.exceptionOrNull()}" }
+        }
+        .getOrNull()!!
 }
