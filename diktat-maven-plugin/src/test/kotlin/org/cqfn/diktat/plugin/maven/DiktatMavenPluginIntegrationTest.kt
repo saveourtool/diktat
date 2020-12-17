1ad799e42f2084ecad670ef73ffbc2dee2ef9ec0
Peter Trifanov <peter.trifanov@mail.ru>
2020-12-17 19:53:49 +0300

    Update DiktatMavenPluginIntegrationTest.kt

--- diktat-maven-plugin/src/test/kotlin/org/cqfn/diktat/plugin/maven/DiktatMavenPluginIntegrationTest.kt
+++ diktat-maven-plugin/src/test/kotlin/org/cqfn/diktat/plugin/maven/DiktatMavenPluginIntegrationTest.kt
@@ -10,9 +10,12 @@ import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.readText
 
 /**
- * Integration tests for diktat-maven-plugin.
- * Run against the project from diktat-examples.
- * Note: for maven itf test name should equal example project's directory name, which we have in pom.xml.
+ * Integration tests for diktat-maven-plugin. Run against the project from diktat-examples.
+ * The whole pipeline is as follows:
+ * * For each test case, test data is copied from examples with respect to maven-itf requirements, .mvn/jvm.config is copied too
+ *   Note: for maven itf test name should equal example project's directory name, which we have in pom.xml.
+ * * maven-failsafe-plugin launches tests; for each test case a separate maven process is spawned
+ * * maven execution results are analyzed here; .mvn/jvm.config is used to attach jacoco java agent to every maven process and generate individual execution reports
  */
 @OptIn(ExperimentalPathApi::class)
 @MavenJupiterExtension
