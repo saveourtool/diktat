426dc513f6764cc8e56390ca253aa596efdd7932
Peter Trifanov <peter.trifanov@mail.ru>
2020-12-29 15:18:14 +0300

    Version incremented
    ### What's done:
    * Small fixes

--- diktat-gradle-plugin/build.gradle.kts
+++ diktat-gradle-plugin/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 // default value is needed for correct gradle loading in IDEA; actual value from maven is used during build
 val ktlintVersion = project.properties.getOrDefault("ktlintVersion", "0.39.0") as String
-val diktatVersion = project.version.takeIf { it.toString() != Project.DEFAULT_VERSION } ?: "0.1.7"
+val diktatVersion = project.version.takeIf { it.toString() != Project.DEFAULT_VERSION } ?: "0.2.0"
 val junitVersion = project.properties.getOrDefault("junitVersion", "5.7.0") as String
 val jacocoVersion = project.properties.getOrDefault("jacocoVersion", "0.8.6") as String
 dependencies {
