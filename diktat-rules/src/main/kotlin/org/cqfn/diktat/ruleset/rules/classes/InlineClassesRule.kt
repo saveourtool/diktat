f4d0ca442c5293352d59faad5e39d79e01e3422c
aktsay6 <aktsay6@gmail.com>
2021-01-21 17:35:57 +0300

    feature/inline-classes(#698)
    ### What's done:
      * Fixed bugs

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/classes/InlineClassesRule.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/classes/InlineClassesRule.kt
@@ -65,12 +65,11 @@ class InlineClassesRule(private val configRule: List<RulesConfig>) : Rule("inlin
     }
 
     private fun isExtendingClass(node: ASTNode): Boolean =
-        node
-            .getFirstChildWithType(SUPER_TYPE_LIST)
-            ?.children()
-            ?.any { it.hasChildOfType(CONSTRUCTOR_CALLEE) }
-            ?: false
-
+            node
+                .getFirstChildWithType(SUPER_TYPE_LIST)
+                ?.children()
+                ?.any { it.hasChildOfType(CONSTRUCTOR_CALLEE) }
+                ?: false
 
     companion object {
         val goodModifiers = listOf(PUBLIC_KEYWORD, PRIVATE_KEYWORD, FINAL_KEYWORD, PROTECTED_KEYWORD, INTERNAL_KEYWORD)
