d3652797578cb0828c51f70856de2f9b4ef55bed
kentr0w <qbextted0@gmail.com>
2021-02-03 16:44:10 +0300

    Top level order
    ### What's done:
       Fixed according to our code style

--- diktat-gradle-plugin/src/main/kotlin/org/cqfn/diktat/plugin/gradle/Utils.kt
+++ diktat-gradle-plugin/src/main/kotlin/org/cqfn/diktat/plugin/gradle/Utils.kt
@@ -9,7 +9,7 @@ package org.cqfn.diktat.plugin.gradle
 import groovy.lang.Closure
 
 @Suppress("MISSING_KDOC_TOP_LEVEL", "MISSING_KDOC_CLASS_ELEMENTS", "KDOC_NO_CONSTRUCTOR_PROPERTY",
-    "MISSING_KDOC_ON_FUNCTION", "KDOC_WITHOUT_PARAM_TAG", "KDOC_WITHOUT_RETURN_TAG")
+    "MISSING_KDOC_ON_FUNCTION", "KDOC_WITHOUT_PARAM_TAG", "KDOC_WITHOUT_RETURN_TAG", "MatchingDeclarationName")
 class KotlinClosure1<in T : Any?, V : Any>(
     val function: T.() -> V?,
     owner: Any? = null,
@@ -23,4 +23,4 @@ class KotlinClosure1<in T : Any?, V : Any>(
 // Because `kotlin-dsl` depends on kotlin 1.3.x.
 @Suppress("MISSING_KDOC_TOP_LEVEL", "MISSING_KDOC_ON_FUNCTION", "KDOC_WITHOUT_PARAM_TAG", "KDOC_WITHOUT_RETURN_TAG")
 fun <T> Any.closureOf(action: T.() -> Unit): Closure<Any?> =
-    KotlinClosure1(action, this, this)
+        KotlinClosure1(action, this, this)
--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/utils/StringCaseUtils.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/utils/StringCaseUtils.kt
@@ -1,4 +1,4 @@
-@file:Suppress("FILE_NAME_MATCH_CLASS", "MatchingDeclarationName")
+@file:Suppress("FILE_NAME_MATCH_CLASS")
 
 package org.cqfn.diktat.ruleset.utils
 
@@ -7,6 +7,7 @@ import com.google.common.base.CaseFormat
 /**
  * Available cases to name enum members
  */
+@Suppress("MatchingDeclarationName")
 enum class Style {
     PASCAL_CASE,
     SNAKE_CASE,
