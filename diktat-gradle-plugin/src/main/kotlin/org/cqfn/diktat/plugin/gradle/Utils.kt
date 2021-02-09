140f3db2ef68f4a24810ac8715f4968d5b65277a
aktsay6 <aktsay6@gmail.com>
2021-02-09 16:52:08 +0300

    bugfix/newlinesrule(#748)
    ### What's done:
      * Fixed bugs

--- diktat-gradle-plugin/src/main/kotlin/org/cqfn/diktat/plugin/gradle/Utils.kt
+++ diktat-gradle-plugin/src/main/kotlin/org/cqfn/diktat/plugin/gradle/Utils.kt
@@ -27,6 +27,11 @@ class KotlinClosure1<in T : Any?, V : Any>(
 
 // These two are copy-pasted from `kotlin-dsl` plugin's groovy interop.
 // Because `kotlin-dsl` depends on kotlin 1.3.x.
-@Suppress("MISSING_KDOC_TOP_LEVEL", "MISSING_KDOC_ON_FUNCTION", "KDOC_WITHOUT_PARAM_TAG", "KDOC_WITHOUT_RETURN_TAG")
+@Suppress(
+    "MISSING_KDOC_TOP_LEVEL",
+    "MISSING_KDOC_ON_FUNCTION",
+    "KDOC_WITHOUT_PARAM_TAG",
+    "KDOC_WITHOUT_RETURN_TAG"
+)
 fun <T> Any.closureOf(action: T.() -> Unit): Closure<Any?> =
         KotlinClosure1(action, this, this)
