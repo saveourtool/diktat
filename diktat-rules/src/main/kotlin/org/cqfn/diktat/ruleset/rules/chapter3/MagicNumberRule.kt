4bf0edd83a8b57c58f0fa3aeb1c289023bbffa0f
kentr0w <qbextted0@gmail.com>
2021-02-13 22:42:39 +0300

    Magic Number
    ### What's done:
       Fixed according to our code style

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/chapter3/MagicNumberRule.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/chapter3/MagicNumberRule.kt
@@ -34,37 +34,39 @@ class MagicNumberRule(configRules: List<RulesConfig>) : DiktatRule("magic-number
         val configuration = MagicNumberConfiguration(
             configRules.getRuleConfig(MAGIC_NUMBER)?.configuration ?: emptyMap()
         )
-        if (node.elementType == INTEGER_CONSTANT || node.elementType == FLOAT_CONSTANT){
+        if (node.elementType == INTEGER_CONSTANT || node.elementType == FLOAT_CONSTANT) {
             checkNumber(node, configuration)
         }
     }
 
     @Suppress("ComplexMethod")
     private fun checkNumber(node: ASTNode, configuration: MagicNumberConfiguration) {
-        val nodeText = node.treePrev?.let { if(it.elementType == OPERATION_REFERENCE && it.hasChildOfType(MINUS)) "-${node.text}" else node.text} ?: node.text
+        val nodeText = node.treePrev?.let { if (it.elementType == OPERATION_REFERENCE && it.hasChildOfType(MINUS)) "-${node.text}" else node.text } ?: node.text
         val isIgnoreNumber = configuration.ignoreNumbers.contains(nodeText)
-        val isHashFunction = node.parent({it.elementType == FUN && it.isHashFun()}) != null &&
-                node.parents().find {it.elementType == PROPERTY} == null
-        val isPropertyDeclaration = node.parent({it.elementType == PROPERTY && !it.isNodeFromCompanionObject()}) != null
-        val isLocalVariable = node.parent({it.isVarProperty() && (it.psi as KtProperty).isLocal}) != null
-        val isConstant = node.parent({it.elementType == PROPERTY && it.isConstant()}) != null
-        val isCompanionObjectProperty = node.parent({it.elementType == PROPERTY && it.isNodeFromCompanionObject()}) != null
-        val isEnums = node.parent({it.elementType == ENUM_ENTRY}) != null
-        val isRanges = node.treeParent.run { this.elementType == BINARY_EXPRESSION &&
-                this.findChildByType(OPERATION_REFERENCE)?.hasChildOfType(RANGE) ?: false }
-        val isExtensionFunctions = node.parent({it.elementType == FUN && (it.psi as KtFunction).isExtensionDeclaration()}) != null &&
-                node.parents().find {it.elementType == PROPERTY} == null
+        val isHashFunction = node.parent({ it.elementType == FUN && it.isHashFun() }) != null &&
+                node.parents().find { it.elementType == PROPERTY } == null
+        val isPropertyDeclaration = node.parent({ it.elementType == PROPERTY && !it.isNodeFromCompanionObject() }) != null
+        val isLocalVariable = node.parent({ it.isVarProperty() && (it.psi as KtProperty).isLocal }) != null
+        val isConstant = node.parent({ it.elementType == PROPERTY && it.isConstant() }) != null
+        val isCompanionObjectProperty = node.parent({ it.elementType == PROPERTY && it.isNodeFromCompanionObject() }) != null
+        val isEnums = node.parent({ it.elementType == ENUM_ENTRY }) != null
+        val isRanges = node.treeParent.run {
+            this.elementType == BINARY_EXPRESSION &&
+                this.findChildByType(OPERATION_REFERENCE)?.hasChildOfType(RANGE) ?: false
+        }
+        val isExtensionFunctions = node.parent({ it.elementType == FUN && (it.psi as KtFunction).isExtensionDeclaration() }) != null &&
+                node.parents().find { it.elementType == PROPERTY } == null
         val result = listOf(isHashFunction, isPropertyDeclaration, isLocalVariable, isConstant,
-            isCompanionObjectProperty, isEnums, isRanges, isExtensionFunctions).zip(mapConfiguration.map {configuration.getParameter(it.key)})
-        if(result.any {it.first && it.first != it.second} && !isIgnoreNumber) {
+            isCompanionObjectProperty, isEnums, isRanges, isExtensionFunctions).zip(mapConfiguration.map { configuration.getParameter(it.key) })
+        if (result.any { it.first && it.first != it.second } && !isIgnoreNumber) {
             MAGIC_NUMBER.warn(configRules, emitWarn, isFixMode, nodeText, node.startOffset, node)
         }
     }
 
     private fun ASTNode.isHashFun() =
-        (this.psi as KtFunction).run {
-            this.nameIdentifier?.text == "hashCode" && this.annotationEntries.map { it.text }.contains("@Override")
-        }
+            (this.psi as KtFunction).run {
+                this.nameIdentifier?.text == "hashCode" && this.annotationEntries.map { it.text }.contains("@Override")
+            }
 
     /**
      * [RuleConfiguration] for configuration
@@ -75,13 +77,17 @@ class MagicNumberRule(configRules: List<RulesConfig>) : DiktatRule("magic-number
          */
         val ignoreNumbers = config["ignoreNumbers"]?.split(",")?.map { it.trim() } ?: ignoreNumbersList
 
+        /**
+         * @param param parameter from config
+         * @return value of parameter
+         */
         @Suppress("UnsafeCallOnNullableType")
-        fun getParameter(param: String): Boolean = config[param]?.toBoolean() ?: mapConfiguration[param]!!
+        fun getParameter(param: String) = config[param]?.toBoolean() ?: mapConfiguration[param]!!
     }
 
     companion object {
         val ignoreNumbersList = listOf("-1", "1", "0", "2")
-        val mapConfiguration  = mapOf(
+        val mapConfiguration = mapOf(
             "ignoreHashCodeFunction" to true,
             "ignorePropertyDeclaration" to false,
             "ignoreLocalVariableDeclaration" to false,
