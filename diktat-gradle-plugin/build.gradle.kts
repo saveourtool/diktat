3a4e6a33f868f870d02ca1be80beaf88253e59c8
Peter Trifanov <peter.trifanov@mail.ru>
2021-05-11 11:03:36 +0300

    Update build.gradle.kts

--- diktat-gradle-plugin/build.gradle.kts
+++ diktat-gradle-plugin/build.gradle.kts
@@ -16,7 +16,6 @@ repositories {
     }
     mavenLocal()  // to use snapshot diktat
     mavenCentral()
-    maven("https://oss.sonatype.org/content/repositories/snapshots")  // for jacoco snapshot
 }
 
 // default value is needed for correct gradle loading in IDEA; actual value from maven is used during build
