2201861e8c01ae0effd47e7e20a215834339f3db
Peter Trifanov <peter.trifanov@mail.ru>
2021-06-03 12:06:22 +0300

    Do not remove return type from recursive functions when converting to expression body (#905)
    ### What's done:
    * Changed logic
    * Added test

--- diktat-rules/src/test/resources/test/paragraph3/newlines/ExpressionBodyExpected.kt
+++ diktat-rules/src/test/resources/test/paragraph3/newlines/ExpressionBodyExpected.kt
@@ -1,3 +1,15 @@
 package test.paragraph3.newlines
 
 fun foo() = "lorem ipsum"
+
+fun foo() = "lorem ipsum"
+
+fun foo() = "lorem ipsum"
+
+fun recFoo(): String = "lorem " + recFoo()
+
+fun recFoo():String = "lorem " + recFoo()
+
+fun recFoo(): String = "lorem " + recFoo()
+
+fun foo() = "lorem ipsum"
--- diktat-rules/src/test/resources/test/paragraph3/newlines/ExpressionBodyTest.kt
+++ diktat-rules/src/test/resources/test/paragraph3/newlines/ExpressionBodyTest.kt
@@ -3,3 +3,25 @@ package test.paragraph3.newlines
 fun foo(): String {
     return "lorem ipsum"
 }
+
+fun foo():String{
+    return "lorem ipsum"
+}
+
+fun foo() : String {
+    return "lorem ipsum"
+}
+
+fun recFoo(): String {
+    return "lorem " + recFoo()
+}
+
+fun recFoo():String {
+    return "lorem " + recFoo()
+}
+
+fun recFoo(): String{
+    return "lorem " + recFoo()
+}
+
+fun foo() = "lorem ipsum"
