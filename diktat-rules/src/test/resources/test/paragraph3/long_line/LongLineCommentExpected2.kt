4112a2b231bc73b37ec3e034cd3be5b283531022
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-06-16 15:25:39 +0300

    Bugfix in the LineLength rule (#929)
    ### What's done:
    * Fix bug with NPE in long lines in case when comment located at the line length limit
    * Add tests

---
+++ diktat-rules/src/test/resources/test/paragraph3/long_line/LongLineCommentExpected2.kt
@@ -0,0 +1,10 @@
+/**
+ * This rule checks if there is a backing property for field with property accessors, in case they don't use field keyword
+ */
+class ImplicitBackingPropertyRuleTest(configRules: List<RulesConfig>) {
+    private fun validateAccessors(node: ASTNode, propsWithBackSymbol: List<String>) {
+        val accessors = node.findAllDescendantsWithSpecificType(PROPERTY_ACCESSOR).filter { it.hasChildOfType(BLOCK) }  // Comment, which shouldn't be moved
+        accessors.filter { it.hasChildOfType(GET_KEYWORD) }.forEach { handleGetAccessors(it, node, propsWithBackSymbol) }
+        accessors.filter { it.hasChildOfType(SET_KEYWORD) }.forEach { handleSetAccessors(it, node, propsWithBackSymbol) }
+    }
+}
