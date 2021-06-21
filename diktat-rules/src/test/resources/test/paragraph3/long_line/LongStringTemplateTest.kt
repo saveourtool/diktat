8e043382f8557e3919d5930ce8ae201b04723041
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-06-21 14:54:48 +0300

    Bugfix of NPE in LongLine rule on Binary Expressions (#932)
    ### What's done:
    * Fix bug with NPE: we didn't consider, that offset changes in course of fixes, so it's break the logic sometimes
    * Add additional check for long comments: if there is only one white space, which is located after // - we shouldn't fix split it
    * Add tests

---
+++ diktat-rules/src/test/resources/test/paragraph3/long_line/LongStringTemplateTest.kt
@@ -0,0 +1,10 @@
+object Observables {
+    val someCode = 15 // looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongggggggggggggggggggggggggg
+    // Some looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong line
+    @Deprecated("New type inference algorithm in Kotlin 1.4 makes this method obsolete. Method will be removed in future RxKotlin release.",
+        replaceWith = ReplaceWith("Observable.combineLatest(source1, source2, source3, source4, combineFunction)", "io.reactivex.Observable"),
+        level = DeprecationLevel.WARNING)
+    @CheckReturnValue
+    @SchedulerSupport(SchedulerSupport.NONE)
+    inline fun <T1 : Any, T2 : Any, T3 : Any, T4 : Any, R : Any> combineLatest() {}
+}
