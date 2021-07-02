70066f60425d7a092427e783e278fbfa40bebe83
Peter Trifanov <peter.trifanov@mail.ru>
2021-07-02 13:59:40 +0300

    Don't use PSI for line number calculation (#950)
    ### What's done:
    * Changed logic
    * Added test

---
+++ diktat-rules/src/test/resources/test/smoke/src/main/kotlin/LocalVariableWithOffsetExpected.kt
@@ -0,0 +1,9 @@
+package org.cqfn.diktat
+
+override fun boo() {
+    val listTestResult: MutableList<TestResult> = mutableListOf()
+    files.chunked(warnPluginConfig.batchSize ?: 1).map { chunk ->
+        handleTestFile(chunk.map { it.single() }, warnPluginConfig, generalConfig)
+    }.forEach { listTestResult.addAll(it) }
+}
+
