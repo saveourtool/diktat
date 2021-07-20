6dc1b95c4988645a7dfc3ee7c85c01d5d0498705
kentr0w <qbextted0@gmail.com>
2021-07-20 17:48:57 +0300

    Range to until
    ### What's done:
    Fixed according to our code style

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/RangeConventionalRuleWarnTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/RangeConventionalRuleWarnTest.kt
@@ -1,12 +1,12 @@
 package org.cqfn.diktat.ruleset.chapter3
 
+import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.cqfn.diktat.ruleset.constants.Warnings
 import org.cqfn.diktat.ruleset.rules.DIKTAT_RULE_SET_ID
 import org.cqfn.diktat.ruleset.rules.chapter3.RangeConventionalRule
 import org.cqfn.diktat.util.LintTestBase
 
 import com.pinterest.ktlint.core.LintError
-import org.cqfn.diktat.common.config.rules.RulesConfig
 import org.junit.jupiter.api.Test
 
 class RangeConventionalRuleWarnTest : LintTestBase(::RangeConventionalRule) {
