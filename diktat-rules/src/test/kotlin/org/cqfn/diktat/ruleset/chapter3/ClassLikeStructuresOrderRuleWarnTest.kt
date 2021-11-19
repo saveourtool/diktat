6cb159dec292dd06dd43d406ef91a1988e8ab32a
sanyavertolet <alexander.frolov@huawei.com>
2021-11-19 15:45:04 +0300

    Bugfix for misleading error message
    ### What's done:
     * Changed logging message
     * Added a test
    
    (#1044)

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/ClassLikeStructuresOrderRuleWarnTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/chapter3/ClassLikeStructuresOrderRuleWarnTest.kt
@@ -213,19 +213,4 @@ class ClassLikeStructuresOrderRuleWarnTest : LintTestBase(::ClassLikeStructuresO
             """.trimMargin()
         )
     }
-
-    @Test
-    @Tag(WarningNames.BLANK_LINE_BETWEEN_PROPERTIES)
-    fun `misleading error message test`() {
-        lintMethod(
-            """
-                |class A {
-                |    val bar: Bar
-                |    @Autowried
-                |    val fileSystemRepository: Foo
-                }
-            """.trimMargin(),
-            LintError(3, 5, ruleId, "${BLANK_LINE_BETWEEN_PROPERTIES.warnText()} @Autowried", true)
-        )
-    }
 }
