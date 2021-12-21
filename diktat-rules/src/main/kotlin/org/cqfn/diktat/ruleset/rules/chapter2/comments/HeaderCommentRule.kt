da3f5aea3e8b92452244a763d0e732395eda3bbf
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-12-21 20:00:40 +0700

    Improvements for Copyrights rule - support pattern for the current year in the configuration (#1154)
    ### What's done:
    * Improvements for Copyrights rule - support pattern for the current year in the configuration

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/chapter2/comments/HeaderCommentRule.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/chapter2/comments/HeaderCommentRule.kt
@@ -135,12 +135,12 @@ class HeaderCommentRule(configRules: List<RulesConfig>) : DiktatRule(
             return
         }
 
-        if (makeCopyrightCorrectYear(configuration.getCopyrightText()).isNotEmpty()) {
-            log.warn("Copyright year is not up do date.")
-        }
-
         // need to make sure that copyright year is consistent with current year
         val copyrightText = configuration.getCopyrightText()
+
+        if (makeCopyrightCorrectYear(copyrightText).isNotEmpty()) {
+            log.warn("Copyright year is not up do date.")
+        }
 
         val headerComment = node.findChildBefore(PACKAGE_DIRECTIVE, BLOCK_COMMENT)
         // Depends only on content and doesn't consider years
@@ -234,11 +234,13 @@ class HeaderCommentRule(configRules: List<RulesConfig>) : DiktatRule(
         /**
          * @return text of copyright as configured in the configuration file
          */
-        fun getCopyrightText() = config["copyrightText"] ?: error("Copyright is not set in configuration")
+        fun getCopyrightText() = config["copyrightText"]?.replace(CURR_YEAR_PATTERN, curYear.toString())
+            ?: error("Copyright is not set in configuration")
     }
 
     companion object {
         private val log = LoggerFactory.getLogger(HeaderCommentRule::class.java)
+        const val CURR_YEAR_PATTERN = ";@currYear;"
         val hyphenRegex = Regex("""\d+-\d+""")
         val afterCopyrightRegex = Regex("""((©|\([cC]\))+ *\d+)""")
         val curYear = LocalDate.now().year
