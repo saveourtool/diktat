f7c9c8a31a04b7f8f6ecea5e3483139b2005c233
Nariman Abdullin <nulls.narik@gmail.com>
2022-05-27 16:32:14 +0300

    Reused c.p.k.c.Rule.VisitorModifier.RunAfterRule (#1310)
    ### What's done:
    * added a wrapper for rule with c.p.k.c.Rule.VisitorModifier.RunAfterRule is pointed to prev rule
    * added test for OrderedRule

--- .git-hooks/commit-msg.sh
+++ .git-hooks/commit-msg.sh
@@ -2,8 +2,9 @@
 
 commit_pattern="(Merge (remote-tracking )?branch|### What's done:)"
 error_msg="Your commit message doesn't match the pattern $commit_pattern. Please fix it."
+commit_count="$(git rev-list --count master..HEAD 2> /dev/null)"
 
-if [[ ! $( cat "$1" ) =~ $commit_pattern ]]
+if [[ $commit_count = "0" && ! $( cat "$1" ) =~ $commit_pattern ]]
 then
     echo "$error_msg"
     exit 1
