93a931dc94b9dc8b466a649c48ee8f1563a1987a
Nariman Abdullin <nulls.narik@gmail.com>
2022-05-27 16:47:10 +0300

    Supported KDoc
    ### What's done:
    * amend new line on tree level

--- diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/chapter3/EnumsSeparated.kt
+++ diktat-rules/src/main/kotlin/org/cqfn/diktat/ruleset/rules/chapter3/EnumsSeparated.kt
@@ -50,20 +50,11 @@ class EnumsSeparated(configRules: List<RulesConfig>) : DiktatRule(
             if (!it.treeNext.isWhiteSpaceWithNewline()) {
                 ENUMS_SEPARATED.warnAndFix(configRules, emitWarn, isFixMode, "enum entries must end with a line break",
                     it.startOffset, it) {
-                    it.appendNewline()
+                    it.treeParent.addChild(PsiWhiteSpaceImpl("\n"), it.treeNext)
                 }
             }
         }
         checkLastEnum(enumEntries.last())
-    }
-
-    private fun ASTNode.appendNewline() {
-        val nextNode = this.treeNext
-        if (nextNode?.elementType == WHITE_SPACE) {
-            (nextNode as LeafPsiElement).rawReplaceWithText("\n${nextNode.text}")
-        } else {
-            this.treeParent.addChild(PsiWhiteSpaceImpl("\n"), nextNode)
-        }
     }
 
     private fun isEnumOneLine(nodes: List<ASTNode>) =
