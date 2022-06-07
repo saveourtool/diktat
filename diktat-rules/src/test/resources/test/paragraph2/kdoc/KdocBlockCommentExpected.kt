7d6fe8e77ced29c264d22490b81fbc93ba956490
Dmitry Morozovsky <27895587+icemachined@users.noreply.github.com>
2022-06-07 14:10:23 +0300

    add uncommitted files

---
+++ diktat-rules/src/test/resources/test/paragraph2/kdoc/KdocBlockCommentExpected.kt
@@ -0,0 +1,20 @@
+package test.paragraph2.kdoc
+
+/**
+ * right place for kdoc
+ */
+class Example {
+    /**
+     * right place for kdoc
+     */
+    fun doGood() {
+        /*
+         * wrong place for kdoc
+         */
+        /*
+         * right place for block comment
+        */
+        // right place for eol comment
+        1 + 2
+    }
+}
