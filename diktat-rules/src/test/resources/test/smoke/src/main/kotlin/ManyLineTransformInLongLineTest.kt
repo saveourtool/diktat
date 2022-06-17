4d4532a30f856cb3a6544a3fd7520379c4bd5b7c
Arrgentum <alexeyvotintsev@yandex.ru>
2022-06-17 17:04:11 +0300

    ### Whats added:  * correct logic check White Space around Operator References in WhiteSpaceRule.kt  * added smoke test  * fix all
    ### Issue(#1339)

--- diktat-rules/src/test/resources/test/smoke/src/main/kotlin/ManyLineTransformInLongLineTest.kt
+++ diktat-rules/src/test/resources/test/smoke/src/main/kotlin/ManyLineTransformInLongLineTest.kt
@@ -32,15 +32,14 @@ fun foo(){
             or 29
             or 30
             or 31
-            or 32
+            ?: 32
             or 33
             or 34
             or 35
             or 36
             or 37
             or 38
-            or 39
-            or 40
+            or 39 ?: 40
             or 41
             or 42
             or 43
@@ -54,25 +53,25 @@ fun foo(){
             or 51
             or 52
             or 53
-            or 54
+            + 54
             or 55
             or 56
             or 57
             or 58
             or 59
-            or 60
+            ?: 60
             or 61
             or 62
             or 63
             or 64
             or 65
             or 66
-            or 67
+            - 67
             or 68
             or 69
             or 70
             or 1
-            or 2
+            + 2
             or 3
             or 4
             or 5
@@ -89,8 +88,8 @@ fun foo(){
             or 16
             or 17
             or 18
-            or 19
-            or 20
+            + 19
+            - 20
             or 21
             or 22
             or 23
@@ -112,15 +111,15 @@ fun foo(){
             or 39
             or 40
             or 41
-            or 42
+            || 42
             or 43
             or 44
             or 45
             or 46
             or 47
-            or 48
+            && 48
             or 49
-            or 50
+            || 50
             or 51
             or 52
             or 53
