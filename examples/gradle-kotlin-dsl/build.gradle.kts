812b24dd4ed687e3f5f724a1ae85f2e81d516330
Nariman Abdullin <nulls.narik@gmail.com>
2022-06-21 10:47:55 +0300

    reverted not-required changes

--- examples/gradle-kotlin-dsl/build.gradle.kts
+++ examples/gradle-kotlin-dsl/build.gradle.kts
@@ -1,16 +1,9 @@
 plugins {
     id("org.cqfn.diktat.diktat-gradle-plugin") version "1.1.0"
-    kotlin("jvm") version "1.7.0"
 }
 
 repositories {
     mavenCentral()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += listOf("-Xuse-k2")
-    }
 }
 
 diktat {
