ab6461034944a8f65ac1847ca612cbad89d2ec6e
Nariman Abdullin <nulls.narik@gmail.com>
2022-06-22 11:28:08 +0300

    added test for ktlint and ordering

--- diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/rules/OrderedRuleSetTest.kt
+++ diktat-rules/src/test/kotlin/org/cqfn/diktat/ruleset/rules/OrderedRuleSetTest.kt
@@ -1,7 +1,10 @@
 package org.cqfn.diktat.ruleset.rules
 
 import org.cqfn.diktat.ruleset.constants.EmitType
+import org.cqfn.diktat.util.TEST_FILE_NAME
+import com.pinterest.ktlint.core.KtLint
 import com.pinterest.ktlint.core.Rule
+import com.pinterest.ktlint.core.api.FeatureInAlphaState
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -10,28 +13,12 @@ class OrderedRuleSetTest {
 
     @Test
     fun `check OrderedRule with VisitorModifier RunAfterRule`() {
-        val rule = object : Rule("rule") {
-            override fun visit(
-                node: ASTNode,
-                autoCorrect: Boolean,
-                emit: EmitType
-            ) {
-                // do nothing
-            }
-        }
+        val rule = mockRule("rule")
         Assertions.assertThrows(IllegalArgumentException::class.java) {
             OrderedRuleSet("duplicate", rule, rule)
         }
 
-        val ruleWithRunAfterRule = object : Rule("invalid-rule", setOf(VisitorModifier.RunAfterRule("another-rule"))) {
-            override fun visit(
-                node: ASTNode,
-                autoCorrect: Boolean,
-                emit: EmitType
-            ) {
-                // do nothing
-            }
-        }
+        val ruleWithRunAfterRule = mockRule("invalid-rule", setOf(Rule.VisitorModifier.RunAfterRule("another-rule")))
         // validate that second rule which will be modified doesn't contain VisitorModifier.RunAfterRule
         Assertions.assertThrows(IllegalArgumentException::class.java) {
             OrderedRuleSet("visitor-modifier", rule, ruleWithRunAfterRule)
@@ -44,24 +31,8 @@ class OrderedRuleSetTest {
 
     @Test
     fun `check OrderedRule`() {
-        val rule1 = object : Rule("rule-first") {
-            override fun visit(
-                node: ASTNode,
-                autoCorrect: Boolean,
-                emit: EmitType
-            ) {
-                // do nothing
-            }
-        }
-        val rule2 = object : Rule("rule-second") {
-            override fun visit(
-                node: ASTNode,
-                autoCorrect: Boolean,
-                emit: EmitType
-            ) {
-                // do nothing
-            }
-        }
+        val rule1 = mockRule("rule-first")
+        val rule2 = mockRule("rule-second")
 
         val ruleSetId = "id"
         val orderedRuleSet = OrderedRuleSet(ruleSetId, rule1, rule2)
@@ -84,5 +55,55 @@ class OrderedRuleSetTest {
                 Assertions.assertEquals(ruleSetId + ":" + rule1.id, it.ruleId,
                     "Invalid ruleId in Rule.VisitorModifier.RunAfterRule")
             }
+    }
+
+    @Test
+    @OptIn(FeatureInAlphaState::class)
+    fun `KtLint keeps order with RuleVisitorModifierRunAfterRule`() {
+        val code = """
+            fun foo() {
+            }
+        """.trimIndent()
+
+        val ruleIdOrder = mutableListOf<String>()
+        val onVisit: (Rule) -> Unit = {
+            ruleIdOrder.add(it.id)
+        }
+        val rule1 = mockRule("ccc", onVisit = onVisit)
+        val rule2 = mockRule("bbb", onVisit = onVisit)
+        val rule3 = mockRule("aaa", onVisit = onVisit)
+        val expectedRuleIdOrder = listOf(rule1, rule2, rule3).map { it.id }
+
+        val ruleSet = OrderedRuleSet("id", rule1, rule2, rule3)
+
+        KtLint.lint(
+            KtLint.ExperimentalParams(
+                fileName = TEST_FILE_NAME,
+                text = code,
+                ruleSets = listOf(ruleSet),
+                cb = { _, _ -> },
+            )
+        )
+
+        val rulesSize = expectedRuleIdOrder.size
+        Assertions.assertTrue(ruleIdOrder.size % rulesSize == 0, "Rules are called several times but together")
+        for (repeat in 0 until (ruleIdOrder.size % rulesSize)) {
+            // check each run for each node
+            Assertions.assertEquals(expectedRuleIdOrder, ruleIdOrder.subList(repeat * rulesSize, (repeat + 1) * rulesSize))
+        }
+    }
+
+    companion object {
+        private fun mockRule(id: String,
+                             visitorModifiers: Set<Rule.VisitorModifier> = emptySet(),
+                             onVisit: (Rule) -> Unit = { }) = object : Rule(id, visitorModifiers) {
+            override fun visit(
+                node: ASTNode,
+                autoCorrect: Boolean,
+                emit: EmitType
+            ) {
+                onVisit(this)
+            }
+        }
     }
 }
