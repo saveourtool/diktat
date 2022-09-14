02b39609491858fba8963859744f7c34e5b0b333
Nariman Abdullin <nulls.narik@gmail.com>
2022-09-14 15:50:11 +0300

    Fix simplification of boolean expression
    ### What's done:
    * added test
    
    It closes #1521

---
+++ diktat-rules/src/test/resources/test/paragraph3/boolean_expressions/ExpressionSimplificationExpected.kt
@@ -0,0 +1,8 @@
+package test.paragraph3.boolean_expressions
+
+fun F.foo() {
+    if (this.valueParameters[i].getFunctionName() != other.valueParameters[i].getFunctionName() || this.valueParameters[i].getFunctionType() != other.valueParameters[i].getFunctionType()
+    ) {
+        return false
+    }
+}
---
+++ diktat-rules/src/test/resources/test/paragraph3/boolean_expressions/ExpressionSimplificationTest.kt
@@ -0,0 +1,9 @@
+package test.paragraph3.boolean_expressions
+
+fun F.foo() {
+    if (!(this.valueParameters[i].getFunctionName() == other.valueParameters[i].getFunctionName() &&
+                this.valueParameters[i].getFunctionType() == other.valueParameters[i].getFunctionType())
+    ) {
+        return false
+    }
+}
