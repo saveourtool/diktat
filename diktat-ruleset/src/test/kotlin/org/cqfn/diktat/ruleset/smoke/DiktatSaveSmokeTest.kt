0b96349443b5c1a1ee40337e7d9e5bca89fda1cb
Nariman Abdullin <nulls.narik@gmail.com>
2022-10-28 12:04:34 +0300

    removed debug logging

--- diktat-ruleset/src/test/kotlin/org/cqfn/diktat/ruleset/smoke/DiktatSaveSmokeTest.kt
+++ diktat-ruleset/src/test/kotlin/org/cqfn/diktat/ruleset/smoke/DiktatSaveSmokeTest.kt
@@ -87,7 +87,6 @@ class DiktatSaveSmokeTest : DiktatSmokeTestBase() {
                 softly.assertThat(saveLog).isRegularFile
 
                 val saveOutput = saveLog.readText()
-                println(saveOutput)
 
                 val saveCommandLine = processBuilder.command().joinToString(separator = " ")
                 softly.assertThat(saveOutput)
