b54ababe91511d2d560d781f25e89191a518b724
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-11-08 11:11:53 +0300

    WIP ### What's done: * WIP

--- diktat-test-framework/src/main/kotlin/org/cqfn/diktat/test/framework/common/ExecutionResult.kt
+++ diktat-test-framework/src/main/kotlin/org/cqfn/diktat/test/framework/common/ExecutionResult.kt
@@ -3,8 +3,6 @@ package org.cqfn.diktat.test.framework.common
 /**
  * Class that keeps the result of executed command
  *
- * @param stdOut standard output
- * @param stdErr error stream
  * @property stdOut content from stdout stream
  * @property stdErr content from stderr stream
  */
