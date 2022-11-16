887edc556c49d3f5e63425412d806ee13f2aabbb
Nariman Abdullin <nulls.narik@gmail.com>
2022-11-16 14:06:54 +0300

    [skip-ci] WIP #3

--- diktat/src/main/kotlin/org/cqfn/diktat/DiktatMain.kt
+++ diktat/src/main/kotlin/org/cqfn/diktat/DiktatMain.kt
@@ -2,14 +2,17 @@ package org.cqfn.diktat
 
 import org.cqfn.diktat.api.DiktatLogLevel
 import org.cqfn.diktat.api.DiktatReporterType
+import org.cqfn.diktat.common.config.rules.DIKTAT
 import org.cqfn.diktat.common.config.rules.DIKTAT_ANALYSIS_CONF
+import org.cqfn.diktat.common.config.rules.DIKTAT_RULE_SET_ID
+import org.cqfn.diktat.ruleset.rules.DiktatRuleSetProvider
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.default
 
 fun main(args: Array<String>) {
-    val parser = ArgParser("diktat")
-    val configOption = parser.option(
+    val parser = ArgParser(DIKTAT)
+    val config: String by parser.option(
         type = ArgType.String,
         fullName = "config",
         shortName = "c",
@@ -19,40 +22,32 @@ fun main(args: Array<String>) {
             directory is used.
         """.trimIndent(),
     ).default(DIKTAT_ANALYSIS_CONF)
-    val config: String by configOption
-
-    val formatOption = parser.option(
+    val format: Boolean by parser.option(
         type = ArgType.Boolean,
         fullName = "format",
         shortName = "F",
         description = "Fix any deviations from the code style."
     ).default(false)
-    val format: Boolean by formatOption
-
-    val reporterOption = parser.option(
+    val reporter: DiktatReporterType by parser.option(
         type = ArgType.Choice<DiktatReporterType>(),
         fullName = "reporter",
         shortName = "r",
         description = "The reporter to use",
     ).default(DiktatReporterType.PLAIN)
-    val reporter: DiktatReporterType by reporterOption
-
-    val outputOption = parser.option(
+    val output: String? by parser.option(
         type = ArgType.String,
         fullName = "output",
         shortName = "o",
         description = "Redirect the reporter output to a file.",
     )
-    val output: String? by outputOption
-
-    val logLevelOption = parser.option(
+    val logLevel: DiktatLogLevel by parser.option(
         type = ArgType.Choice<DiktatLogLevel>(),
         fullName = "log-level",
         shortName = "l",
-        description = "Enable the output of specified level",
+        description = "Enable the output with specific level",
     ).default(DiktatLogLevel.INFO)
-    val logLevel: DiktatLogLevel by logLevelOption
 
     parser.parse(args)
 
+    val diktatRuleSetProvider = DiktatRuleSetProvider(config)
 }
