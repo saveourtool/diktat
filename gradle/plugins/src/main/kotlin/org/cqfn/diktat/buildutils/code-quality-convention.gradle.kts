e2e2012c4768688b5ace59ca6a4387000c0b0e02
Nariman Abdullin <nulls.narik@gmail.com>
2023-03-16 18:16:25 +0300

    disabled spotless plugin

--- gradle/plugins/src/main/kotlin/org/cqfn/diktat/buildutils/code-quality-convention.gradle.kts
+++ gradle/plugins/src/main/kotlin/org/cqfn/diktat/buildutils/code-quality-convention.gradle.kts
@@ -3,5 +3,5 @@ package org.cqfn.diktat.buildutils
 plugins {
     id("org.cqfn.diktat.buildutils.detekt-convention-configuration")
     id("org.cqfn.diktat.buildutils.diktat-convention-configuration")
-    id("org.cqfn.diktat.buildutils.spotless-convention-configuration")
+//    id("org.cqfn.diktat.buildutils.spotless-convention-configuration")
 }
--- gradle/plugins/src/main/kotlin/org/cqfn/diktat/buildutils/spotless-convention-configuration.gradle.kts
+++ gradle/plugins/src/main/kotlin/org/cqfn/diktat/buildutils/spotless-convention-configuration.gradle.kts
@@ -10,31 +10,30 @@ plugins {
 val libs = the<LibrariesForLibs>()
 val diktatVersion: String = libs.versions.diktat.asProvider().get()
 spotless {
-//    kotlin {
-//        diktat(diktatVersion).configFile(rootProject.file("diktat-analysis.yml"))
-//        target("src/**/*.kt")
-//        targetExclude("src/test/**/*.kt", "src/*Test/**/*.kt")
-//        if (path == rootProject.path) {
-//            target("gradle/plugins/src/**/*.kt")
-//        }
-//    }
-//    kotlinGradle {
-//        ktlint()
-//        diktat(diktatVersion).configFile(rootProject.file("diktat-analysis.yml"))
-//
-//        // using `Project#path` here, because it must be unique in gradle's project hierarchy
-//        if (path == rootProject.path) {
-//            target("$rootDir/*.kts", "$rootDir/gradle/plugins/**/*.kts")
-//            targetExclude(
-//                "$rootDir/build/**/*.kts",
-//                "$rootDir/gradle/plugins/build/**/*.kts",
-//            )
-//        } else {
-//            target("**/*.kts")
-//            targetExclude(
-//                "build/**/*.kts",
-//                "src/test/**/*.kts",
-//            )
-//        }
-//    }
+    kotlin {
+        diktat(diktatVersion).configFile(rootProject.file("diktat-analysis.yml"))
+        target("src/**/*.kt")
+        targetExclude("src/test/**/*.kt", "src/*Test/**/*.kt")
+        if (path == rootProject.path) {
+            target("gradle/plugins/src/**/*.kt")
+        }
+    }
+    kotlinGradle {
+        diktat(diktatVersion).configFile(rootProject.file("diktat-analysis.yml"))
+
+        // using `Project#path` here, because it must be unique in gradle's project hierarchy
+        if (path == rootProject.path) {
+            target("$rootDir/*.kts", "$rootDir/gradle/plugins/**/*.kts")
+            targetExclude(
+                "$rootDir/build/**/*.kts",
+                "$rootDir/gradle/plugins/build/**/*.kts",
+            )
+        } else {
+            target("**/*.kts")
+            targetExclude(
+                "build/**/*.kts",
+                "src/test/**/*.kts",
+            )
+        }
+    }
 }
