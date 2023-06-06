6b38fcd0cbf698abb23b745b42951009ea9439df
Nariman Abdullin <nulls.narik@gmail.com>
2023-06-06 11:32:33 +0300

    Removed usage of `GrgitServicePlugin` (#1682)
    ### What's done:
    - removed GrgitServicePlugin
    - setDefaultInferredScope

--- gradle/plugins/src/main/kotlin/org/cqfn/diktat/buildutils/VersioningConfiguration.kt
+++ gradle/plugins/src/main/kotlin/org/cqfn/diktat/buildutils/VersioningConfiguration.kt
@@ -4,33 +4,38 @@
 
 package org.cqfn.diktat.buildutils
 
-import org.ajoberstar.grgit.gradle.GrgitServiceExtension
-import org.ajoberstar.grgit.gradle.GrgitServicePlugin
+import org.ajoberstar.reckon.core.Scope
 import org.ajoberstar.reckon.gradle.ReckonExtension
 import org.ajoberstar.reckon.gradle.ReckonPlugin
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.internal.storage.file.FileRepository
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getByType
 
 /**
  * Configures reckon plugin for [this] project, should be applied for root project only
  */
 fun Project.configureVersioning() {
     apply<ReckonPlugin>()
-    apply<GrgitServicePlugin>()
 
     // should be provided in the gradle.properties
     configure<ReckonExtension> {
+        setDefaultInferredScope(Scope.MINOR.name)
         snapshots()
         setScopeCalc(calcScopeFromProp())
         setStageCalc(calcStageFromProp())
     }
 
-    val status = project.extensions.getByType<GrgitServiceExtension>()
-        .service
-        .map { it.grgit.repository.jgit.status().call() }
-        .get()
+    val status = FileRepositoryBuilder()
+        .findGitDir(project.rootDir)
+        .setup()
+        .let(::FileRepository)
+        .let(::Git)
+        .status()
+        .call()
+
     if (!status.isClean) {
         logger.warn("git tree is not clean; " +
                 "Untracked files: ${status.untracked}, uncommitted changes: ${status.uncommittedChanges}"
