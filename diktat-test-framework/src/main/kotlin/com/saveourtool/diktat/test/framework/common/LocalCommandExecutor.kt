4f23c712145d734238c81ba9e2d824c88e496e7c
Nariman Abdullin <nulls.narik@gmail.com>
2023-06-13 12:56:02 +0300

    Rewrite StreamGobbler (#1692)

--- diktat-test-framework/src/main/kotlin/com/saveourtool/diktat/test/framework/common/LocalCommandExecutor.kt
+++ diktat-test-framework/src/main/kotlin/com/saveourtool/diktat/test/framework/common/LocalCommandExecutor.kt
@@ -1,13 +1,24 @@
 package com.saveourtool.diktat.test.framework.common
 
 import mu.KotlinLogging
-
 import java.io.IOException
+import java.io.InputStream
+import java.nio.charset.Charset
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
 
 /**
  * Class that wraps shell [command] and can execute it
  */
-class LocalCommandExecutor internal constructor(private val command: String) {
+class LocalCommandExecutor internal constructor(
+    private val command: String,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) {
     /**
      * Execute [command]
      *
@@ -18,17 +29,12 @@ class LocalCommandExecutor internal constructor(private val command: String) {
             log.info { "Executing command: $command" }
             val process = Runtime.getRuntime().exec(command)
             process.outputStream.close()
-            val inputStream = process.inputStream
-            val outputGobbler = StreamGobbler(inputStream, "OUTPUT") { msg, ex ->
-                log.error(ex, msg)
+            return runBlocking {
+                ExecutionResult(
+                    process.inputStream.readLinesAsync("OUTPUT", ioDispatcher).toList(),
+                    process.errorStream.readLinesAsync("ERROR", ioDispatcher).toList(),
+                )
             }
-            outputGobbler.start()
-            val errorStream = process.errorStream
-            val errorGobbler = StreamGobbler(errorStream, "ERROR") { msg, ex ->
-                log.error(ex, msg)
-            }
-            errorGobbler.start()
-            return ExecutionResult(outputGobbler.content, errorGobbler.content)
         } catch (ex: IOException) {
             log.error("Execution of $command failed", ex)
         }
@@ -37,5 +43,22 @@ class LocalCommandExecutor internal constructor(private val command: String) {
 
     companion object {
         private val log = KotlinLogging.logger {}
+
+        /**
+         * @param streamType
+         * @param ioDispatcher
+         * @return [Flow] of strings from input stream
+         */
+        fun InputStream.readLinesAsync(streamType: String, ioDispatcher: CoroutineDispatcher): Flow<String> = flow {
+            try {
+                val bufferedReader = this@readLinesAsync.bufferedReader(Charset.defaultCharset())
+                while (true) {
+                    val line = bufferedReader.readLine() ?: break
+                    emit(line)
+                }
+            } catch (ex: IOException) {
+                log.error(ex) { "Failed to consume and display the input stream of type $streamType." }
+            }
+        }.flowOn(ioDispatcher)
     }
 }
