f92f29d971c9e3bf5c2c2b6adc19eb97a31cf8cc
Nariman Abdullin <nulls.narik@gmail.com>
2023-06-13 12:28:34 +0300

    diktatFix & detektAll

--- diktat-test-framework/src/main/kotlin/com/saveourtool/diktat/test/framework/common/LocalCommandExecutor.kt
+++ diktat-test-framework/src/main/kotlin/com/saveourtool/diktat/test/framework/common/LocalCommandExecutor.kt
@@ -4,6 +4,7 @@ import mu.KotlinLogging
 import java.io.IOException
 import java.io.InputStream
 import java.nio.charset.Charset
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -14,7 +15,10 @@ import kotlinx.coroutines.runBlocking
 /**
  * Class that wraps shell [command] and can execute it
  */
-class LocalCommandExecutor internal constructor(private val command: String) {
+class LocalCommandExecutor internal constructor(
+    private val command: String,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) {
     /**
      * Execute [command]
      *
@@ -25,10 +29,10 @@ class LocalCommandExecutor internal constructor(private val command: String) {
             log.info { "Executing command: $command" }
             val process = Runtime.getRuntime().exec(command)
             process.outputStream.close()
-            return runBlocking(Dispatchers.IO) {
+            return runBlocking {
                 ExecutionResult(
-                    process.inputStream.readLinesAsync("OUTPUT").toList(),
-                    process.errorStream.readLinesAsync("ERROR").toList(),
+                    process.inputStream.readLinesAsync("OUTPUT", ioDispatcher).toList(),
+                    process.errorStream.readLinesAsync("ERROR", ioDispatcher).toList(),
                 )
             }
         } catch (ex: IOException) {
@@ -40,16 +44,21 @@ class LocalCommandExecutor internal constructor(private val command: String) {
     companion object {
         private val log = KotlinLogging.logger {}
 
-        fun InputStream.readLinesAsync(streamType: String): Flow<String> = flow {
-                try {
-                    val bufferedReader = this@readLinesAsync.bufferedReader(Charset.defaultCharset())
-                    while (true) {
-                        val line = bufferedReader.readLine() ?: break
-                        emit(line)
-                    }
-                } catch (ex: IOException) {
-                    log.error(ex) { "Failed to consume and display the input stream of type $streamType." }
+        /**
+         * @param streamType
+         * @param ioDispatcher
+         * @return [Flow] of strings from input stream
+         */
+        fun InputStream.readLinesAsync(streamType: String, ioDispatcher: CoroutineDispatcher): Flow<String> = flow {
+            try {
+                val bufferedReader = this@readLinesAsync.bufferedReader(Charset.defaultCharset())
+                while (true) {
+                    val line = bufferedReader.readLine() ?: break
+                    emit(line)
                 }
-            }.flowOn(Dispatchers.IO)
+            } catch (ex: IOException) {
+                log.error(ex) { "Failed to consume and display the input stream of type $streamType." }
+            }
+        }.flowOn(ioDispatcher)
     }
 }
