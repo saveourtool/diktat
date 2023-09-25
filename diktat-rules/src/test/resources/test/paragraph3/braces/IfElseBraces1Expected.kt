3c4e688b73cce801761ea80f76ccfc5c7f057035
Alexander Drugakov <alexander.drugakov@gmail.com>
2023-09-25 15:47:43 +0300

    Fixed BracesInConditionalsAndLoopsRule (#1750)
    ### What's done:
    - Reworked `insertEmptyBlock()` function for adding empty braces blocks to conditions and `do-while` loop with empty bodies.
    - Added check for braces block existence to avoid duplicate braces blocks.
    - Added tests for conditions including tests for cases of `else if` and cases of empty `if` and `else` bodies.
    
    It's part of #1737

--- diktat-rules/src/test/resources/test/paragraph3/braces/IfElseBraces1Expected.kt
+++ diktat-rules/src/test/resources/test/paragraph3/braces/IfElseBraces1Expected.kt
@@ -27,7 +27,85 @@ fun foo3() {
 fun foo4() {
     if (x > 0) {
     } else {
-    } ;
+    };
+}
+
+fun foo5() {
+    if (x > 0)
+        {
+        foo()
+    } else
+        {
+        bar()
+    }
+}
+
+fun foo6() {
+    if (x > 0) {
+        foo()
+    } else if (y > 0) {
+        abc()
+    } else {
+        bar()
+    }
+}
+
+fun foo7() {
+    if (x > 0)
+        {
+        foo()
+    } else if (y > 0)
+        {
+        abc()
+    } else
+        {
+        bar()
+    }
+}
+
+fun foo8() {
+    if (x > 0) {
+        if (y > 0) foo() else abc()
+    } else {
+        bar()
+    }
+}
+
+fun foo9() {
+    if (x > 0) {
+        foo()
+    } else if (y > 0) {
+        abc()
+    } else {
+        bar()
+    }
+}
+
+fun foo10() {
+    if (x > 0) {
+        foo()
+    } else if (z > 0) {
+        if (y > 0) abc() else qwe()
+    } else {
+        bar()
+    }
+}
+
+fun foo11() {
+    if (x > 0) else bar()
+}
+
+fun foo12() {
+    if (x > 0) {
+        foo()
+    } else {
+    };
+}
+
+fun foo13() {
+    if (x > 0) {
+    } else {
+    };
 }
 
 fun foo() {
