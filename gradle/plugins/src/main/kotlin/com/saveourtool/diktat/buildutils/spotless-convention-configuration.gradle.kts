b965324408eb7a1f3c35e231678e67ba53bb39bf
Nariman Abdullin <nulls.narik@gmail.com>
2023-11-13 14:26:32 +0300

    configured targets for kotlin plugin

--- gradle/plugins/src/main/kotlin/com/saveourtool/diktat/buildutils/spotless-convention-configuration.gradle.kts
+++ gradle/plugins/src/main/kotlin/com/saveourtool/diktat/buildutils/spotless-convention-configuration.gradle.kts
@@ -6,9 +6,19 @@ plugins {
 
 spotless {
     kotlin {
-        target("**/*.kt")
-
-        ktlint()
+        // using `Project#path` here, because it must be unique in gradle's project hierarchy
+        if (path == rootProject.path) {
+            target("gradle/plugins/src/**/*.kt", "*.kts", "gradle/plugins/**/*.kts")
+            targetExclude("gradle/plugins/build/**")
+        } else {
+            target("src/**/*.kt", "**/*.kts")
+            targetExclude(
+                "src/test/**/*.kt",
+                "src/test/**/*.kts",
+                "src/*Test/**/*.kt",
+                "build/**/*.kts",
+            )
+        }
         diktat().configFile(
             rootProject.file("diktat-analysis-old.yml")
         )
