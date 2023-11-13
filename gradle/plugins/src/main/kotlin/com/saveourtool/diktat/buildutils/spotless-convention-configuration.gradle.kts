01d13158a9d05a5f658b7e0c68c68c4b9f731f83
Nariman Abdullin <nulls.narik@gmail.com>
2023-11-13 14:31:40 +0300

    excluded kts

--- gradle/plugins/src/main/kotlin/com/saveourtool/diktat/buildutils/spotless-convention-configuration.gradle.kts
+++ gradle/plugins/src/main/kotlin/com/saveourtool/diktat/buildutils/spotless-convention-configuration.gradle.kts
@@ -8,15 +8,16 @@ spotless {
     kotlin {
         // using `Project#path` here, because it must be unique in gradle's project hierarchy
         if (path == rootProject.path) {
-            target("gradle/plugins/src/**/*.kt", "*.kts", "gradle/plugins/**/*.kts")
-            targetExclude("gradle/plugins/build/**")
+            target("gradle/plugins/src/**/*.kt")
+            targetExclude("gradle/plugins/build/**", "*.kts")
         } else {
-            target("src/**/*.kt", "**/*.kts")
+            target("src/**/*.kt")
             targetExclude(
                 "src/test/**/*.kt",
                 "src/test/**/*.kts",
                 "src/*Test/**/*.kt",
                 "build/**/*.kts",
+                "**/*.kts",
             )
         }
         diktat().configFile(rootProject.file("diktat-analysis.yml"))
