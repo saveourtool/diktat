fca7f53ca2fa57f54b8582de39e15b72b2ea4a4f
Alexander Drugakov <alexander.drugakov@gmail.com>
2023-11-27 18:36:47 +0300

    Tested maven plugin (#1823)
    ### What's done:
    - updated `diktat-analysis.yml` files for all examples.
    - added multi-module maven example.
    - tested single-module and multi-module maven examples.
    - tested githubActions via `-Ddiktat.githubActions=true`.
    - tested reporters.
    
    Closes #1792

---
+++ examples/maven-multiproject/backend/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package incorrect
+
+class incorrectname: Exception() {
+    fun INCORRECT_FUNCTION() {
+        throw Exception()
+    }
+
+    // fun myCommentedFunction() {
+    // }
+
+    val Incorrect_Val = 5
+
+}
---
+++ examples/maven-multiproject/frontend/src/main/kotlin/AnotherMain.kt
@@ -0,0 +1,13 @@
+package incorrect
+
+class anotherincorrectname: Exception() {
+    fun ANOTHER_INCORRECT_FUNCTION() {
+        throw Exception()
+    }
+
+    // fun anotherCommentedFunction() {
+    // }
+
+    val Another_Incorrect_Val = 5
+
+}
