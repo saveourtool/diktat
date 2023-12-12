5898f0193cee6a389e093516ee81cf9bbee8253c
Nariman Abdullin <nulls.narik@gmail.com>
2023-12-12 15:47:03 +0300

    removed -- from windows script and fixed coloring in plain reporter

--- diktat-cli/src/main/script/diktat.cmd
+++ diktat-cli/src/main/script/diktat.cmd
@@ -12,7 +12,7 @@ set "git_url=https://github.com/git-for-windows/git/releases/latest"
 if exist "%git_install_location%" (
 	setlocal
 	set "PATH=%git_install_location%\usr\bin;%PATH%"
-	for /f "usebackq tokens=*" %%p in (`cygpath "%~dpn0"`) do bash --noprofile --norc %%p -- %*
+	for /f "usebackq tokens=*" %%p in (`cygpath "%~dpn0"`) do bash --noprofile --norc %%p %*
 ) else (
 	echo Expecting Git for Windows at %git_install_location%; please install it from %git_url%
 	start %git_url%
