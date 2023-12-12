292189a658b6a2f0e9a9bffe46c01bee185684b8
Nariman Abdullin <nulls.narik@gmail.com>
2023-12-12 12:57:33 +0300

    move test with windows separator to windows only

--- diktat-cli/src/test/kotlin/com/saveourtool/diktat/util/CliUtilsKtTest.kt
+++ diktat-cli/src/test/kotlin/com/saveourtool/diktat/util/CliUtilsKtTest.kt
@@ -1,41 +1,22 @@
 package com.saveourtool.diktat.util
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledOnOs
+import org.junit.jupiter.api.condition.OS
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 import java.nio.file.Path
+import java.nio.file.Paths
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.createDirectory
 import kotlin.io.path.createFile
 import kotlin.io.path.writeText
 
 class CliUtilsKtTest {
-    private fun setupHierarchy(dir: Path) {
-        dir.resolveAndCreateDirectory("folder1")
-            .also { folder1 ->
-                folder1.resolveAndCreateDirectory("subFolder11")
-                    .also { subFolder11 ->
-                        subFolder11.resolveAndCreateFile("Test1.kt")
-                        subFolder11.resolveAndCreateFile("Test2.kt")
-                    }
-                folder1.resolveAndCreateDirectory("subFolder12")
-                    .also { subFolder12 ->
-                        subFolder12.resolveAndCreateFile("Test1.kt")
-                    }
-            }
-        dir.resolveAndCreateDirectory("folder2")
-            .also { folder2 ->
-                folder2.resolveAndCreateFile("Test1.kt")
-                folder2.resolveAndCreateFile("Test2.kt")
-                folder2.resolveAndCreateFile("Test3.kt")
-            }
-    }
-
     @Test
-    fun listByFilesWithLeadingAsterisks(@TempDir tmpDir: Path) {
-        setupHierarchy(tmpDir)
-
+    fun listByFilesWithLeadingAsterisks() {
         Assertions.assertThat(tmpDir.listFiles("**/Test1.kt").toList())
             .containsExactlyInAnyOrder(
                 tmpDir.resolve("folder1").resolve("subFolder11").resolve("Test1.kt"),
@@ -46,9 +27,7 @@ class CliUtilsKtTest {
 
 
     @Test
-    fun listByFilesWithGlobalPath(@TempDir tmpDir: Path) {
-        setupHierarchy(tmpDir)
-
+    fun listByFilesWithGlobalPath() {
         Assertions.assertThat(tmpDir.listFiles("${tmpDir.absolutePathString()}${File.separator}**${File.separator}Test2.kt").toList())
             .containsExactlyInAnyOrder(
                 tmpDir.resolve("folder1").resolve("subFolder11").resolve("Test2.kt"),
@@ -57,31 +36,32 @@ class CliUtilsKtTest {
     }
 
     @Test
-    fun listByFilesWithRelativePath(@TempDir tmpDir: Path) {
-        setupHierarchy(tmpDir)
-
-        val expectedResult = arrayOf(
-            tmpDir.resolve("folder1").resolve("subFolder11").resolve("Test1.kt"),
-            tmpDir.resolve("folder1").resolve("subFolder11").resolve("Test2.kt"),
-        )
+    fun listByFilesWithRelativePath() {
         Assertions.assertThat(tmpDir.listFiles("folder1/subFolder11/*.kt").toList())
-            .containsExactlyInAnyOrder(*expectedResult)
-        Assertions.assertThat(tmpDir.listFiles("folder1\\subFolder11\\*.kt").toList())
-            .containsExactlyInAnyOrder(*expectedResult)
+            .containsExactlyInAnyOrder(
+                tmpDir.resolve("folder1").resolve("subFolder11").resolve("Test1.kt"),
+                tmpDir.resolve("folder1").resolve("subFolder11").resolve("Test2.kt"),
+            )
     }
 
     @Test
-    fun listByFilesWithEmptyResult(@TempDir tmpDir: Path) {
-        setupHierarchy(tmpDir)
+    @EnabledOnOs(OS.WINDOWS)
+    fun listByFilesWithRelativePathWindows() {
+        Assertions.assertThat(tmpDir.listFiles("folder1\\subFolder11\\*.kt").toList())
+            .containsExactlyInAnyOrder(
+                tmpDir.resolve("folder1").resolve("subFolder11").resolve("Test1.kt"),
+                tmpDir.resolve("folder1").resolve("subFolder11").resolve("Test2.kt"),
+            )
+    }
 
+    @Test
+    fun listByFilesWithEmptyResult() {
         Assertions.assertThat(tmpDir.listFiles("**/*.kts").toList())
             .isEmpty()
     }
 
     @Test
-    fun listByFilesWithParentFolder(@TempDir tmpDir: Path) {
-        setupHierarchy(tmpDir)
-
+    fun listByFilesWithParentFolder() {
         Assertions.assertThat(tmpDir.resolve("folder1").listFiles("../*/*.kt").toList())
             .containsExactlyInAnyOrder(
                 tmpDir.resolve("folder2").resolve("Test1.kt"),
@@ -91,9 +71,7 @@ class CliUtilsKtTest {
     }
 
     @Test
-    fun listByFilesWithFolder(@TempDir tmpDir: Path) {
-        setupHierarchy(tmpDir)
-
+    fun listByFilesWithFolder() {
         Assertions.assertThat(tmpDir.listFiles("folder2").toList())
             .containsExactlyInAnyOrder(
                 tmpDir.resolve("folder2").resolve("Test1.kt"),
@@ -111,9 +89,7 @@ class CliUtilsKtTest {
     }
 
     @Test
-    fun listByFilesWithNegative(@TempDir tmpDir: Path) {
-        setupHierarchy(tmpDir)
-
+    fun listByFilesWithNegative() {
         Assertions.assertThat(tmpDir.listFiles("**/*.kt", "!**/subFolder11/*.kt", "!**/Test3.kt").toList())
             .containsExactlyInAnyOrder(
                 tmpDir.resolve("folder1").resolve("subFolder12").resolve("Test1.kt"),
@@ -123,6 +99,33 @@ class CliUtilsKtTest {
     }
 
     companion object {
+        @JvmStatic
+        @TempDir
+        internal var tmpDir: Path = Paths.get("/invalid")
+
+        @BeforeAll
+        @JvmStatic
+        internal fun setupHierarchy() {
+            tmpDir.resolveAndCreateDirectory("folder1")
+                .also { folder1 ->
+                    folder1.resolveAndCreateDirectory("subFolder11")
+                        .also { subFolder11 ->
+                            subFolder11.resolveAndCreateFile("Test1.kt")
+                            subFolder11.resolveAndCreateFile("Test2.kt")
+                        }
+                    folder1.resolveAndCreateDirectory("subFolder12")
+                        .also { subFolder12 ->
+                            subFolder12.resolveAndCreateFile("Test1.kt")
+                        }
+                }
+            tmpDir.resolveAndCreateDirectory("folder2")
+                .also { folder2 ->
+                    folder2.resolveAndCreateFile("Test1.kt")
+                    folder2.resolveAndCreateFile("Test2.kt")
+                    folder2.resolveAndCreateFile("Test3.kt")
+                }
+        }
+
         private fun Path.resolveAndCreateDirectory(name: String): Path = resolve(name).also {
             it.createDirectory()
         }
